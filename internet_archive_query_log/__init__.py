af9a646365f19f9da1a79ed930f72cb6f0337b58
Jan Heinrich Reimer <jan.reimer@student.uni-halle.de>
2022-11-10 18:11:17 +0100

    Change defaults

--- internet_archive_query_log/__init__.py
+++ internet_archive_query_log/__init__.py
@@ -1,3 +1,6 @@
 from pathlib import Path
 
-DATA_DIRECTORY = Path(__file__).parent.parent / "data"
+DATA_DIRECTORY_PATH = Path(__file__).parent.parent / "data"
+# DATA_DIRECTORY_PATH = Path("/mnt/ceph/storage/TODO")
+
+CDX_API_URL = "https://web.archive.org/cdx/search/cdx"
