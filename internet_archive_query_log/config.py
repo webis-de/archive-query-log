7357edb889939358b4aca8d6dee250b9dc1a49e1
Jan Heinrich Reimer <jan.reimer@student.uni-halle.de>
2022-11-14 01:02:22 +0100

    Reformat config

--- internet_archive_query_log/config.py
+++ internet_archive_query_log/config.py
@@ -11,13 +11,26 @@ SOURCES: Mapping[str, Sequence[QuerySource]] = {
         # TODO Test
         # TODO Add all global pages
         #  (see https://www.google.com/supported_domains).
-        QuerySource("google.com/search?", QueryParameter("q")),
+        QuerySource(
+            "google.com/search?",
+            QueryParameter("q"),
+        ),
         # Note that images.google.com and shopping.google.com
         # all redirects to google.com.
-        QuerySource("news.google.com/search?", QueryParameter("q")),
-        QuerySource("play.google.com/store/search?", QueryParameter("q")),
-        QuerySource("photos.google.com/search/", PathSuffix("search/")),
-        QuerySource("podcasts.google.com/search/", PathSuffix("search/")),
+        QuerySource(
+            "news.google.com/search?",
+            QueryParameter("q"),
+        ),
+        QuerySource(
+            "play.google.com/store/search?",
+            QueryParameter("q"),
+        ),
+        QuerySource(
+            "photos.google.com/search/", PathSuffix("search/"),
+        ),
+        QuerySource(
+            "podcasts.google.com/search/", PathSuffix("search/"),
+        ),
         QuerySource(
             "google.com/finance/quote/",
             PathSuffix("finance/quote/"),
@@ -37,20 +50,38 @@ SOURCES: Mapping[str, Sequence[QuerySource]] = {
     ],
     "bing": [
         # TODO Add all global pages.
-        QuerySource("bing.com/search?", QueryParameter("q")),
+        QuerySource(
+            "bing.com/search?",
+            QueryParameter("q"),
+        ),
     ],
     "yahoo": [
         # TODO Test
         # TODO Add all global pages.
-        QuerySource("yahoo.com/search?", QueryParameter("p")),
-        QuerySource("search.yahoo.com/search?", QueryParameter("p")),
-        QuerySource("de.search.yahoo.com/search?", QueryParameter("p")),
+        QuerySource(
+            "yahoo.com/search?",
+            QueryParameter("p"),
+        ),
+        QuerySource(
+            "search.yahoo.com/search?",
+            QueryParameter("p"),
+        ),
+        QuerySource(
+            "de.search.yahoo.com/search?",
+            QueryParameter("p"),
+        ),
     ],
     "duckduckgo": [
-        QuerySource("duckduckgo.com/?", QueryParameter("q")),
+        QuerySource(
+            "duckduckgo.com/?",
+            QueryParameter("q"),
+        ),
     ],
     "internet-archive": [
-        QuerySource("archive.org/search.php?", QueryParameter("query")),
+        QuerySource(
+            "archive.org/search.php?",
+            QueryParameter("query"),
+        ),
     ],
     "wikipedia": [
         QuerySource(
@@ -60,53 +91,97 @@ SOURCES: Mapping[str, Sequence[QuerySource]] = {
         for site in WIKIPEDIA_SITES
     ],
     "amazon": [
-        QuerySource(f"{site}/s?", QueryParameter("k"))
+        QuerySource(f"{site}/s?",
+                    QueryParameter("k"),
+                    )
         for site in AMAZON_SITES
     ],
     "ebay": [
-                QuerySource(f"{site}/?", QueryParameter("_nkw"))
+                QuerySource(f"{site}/?",
+                            QueryParameter("_nkw"),
+                            )
                 for site in EBAY_SITES
             ] + [
 
-                QuerySource(f"{site}/sch/i.html?", QueryParameter("_nkw"))
+                QuerySource(f"{site}/sch/i.html?",
+                            QueryParameter("_nkw"),
+                            )
                 for site in EBAY_SITES
             ],
     "lastfm": [
-        QuerySource("last.fm/search?", QueryParameter("q")),
+        QuerySource(
+            "last.fm/search?",
+            QueryParameter("q"),
+        ),
     ],
     "twitter": [
-        QuerySource("twitter.com/search?", QueryParameter("q")),
+        QuerySource(
+            "twitter.com/search?",
+            QueryParameter("q"),
+        ),
     ],
     "facebook": [
-        QuerySource("facebook.com/search?", QueryParameter("q")),
+        QuerySource(
+            "facebook.com/search?",
+            QueryParameter("q"),
+        ),
     ],
     "pubmed": [
-        QuerySource("pubmed.gov/?", QueryParameter("term")),
-        QuerySource("pubmed.ncbi.nlm.nih.gov/?", QueryParameter("term")),
+        QuerySource(
+            "pubmed.gov/?",
+            QueryParameter("term"),
+        ),
+        QuerySource(
+            "pubmed.ncbi.nlm.nih.gov/?",
+            QueryParameter("term"),
+        ),
     ],
     "google-scholar": [
-        QuerySource("scholar.google.com/scholar?", QueryParameter("q")),
+        QuerySource(
+            "scholar.google.com/scholar?",
+            QueryParameter("q"),
+        ),
     ],
     "dblp": [
-        QuerySource("dblp.org/search?", QueryParameter("q")),
-        QuerySource("dblp.uni-trier.de/search?", QueryParameter("q")),
+        QuerySource(
+            "dblp.org/search?",
+            QueryParameter("q"),
+        ),
+        QuerySource(
+            "dblp.uni-trier.de/search?",
+            QueryParameter("q"),
+        ),
     ],
     "github": [
-        QuerySource("github.com/search?", QueryParameter("q")),
+        QuerySource(
+            "github.com/search?",
+            QueryParameter("q"),
+        ),
     ],
     "stackexchange": [
-        QuerySource(f"{site}/search?", QueryParameter("q"))
+        QuerySource(f"{site}/search?",
+                    QueryParameter("q"),
+                    )
         for site in STACKEXCHANGE_SITES
     ],
     "chatnoir": [
-        QuerySource("chatnoir.eu/?", QueryParameter("q")),
+        QuerySource(
+            "chatnoir.eu/?",
+            QueryParameter("q"),
+        ),
     ],
     "argsme": [
-        QuerySource("args.me/?query=", QueryParameter("query")),
+        QuerySource(
+            "args.me/?query=",
+            QueryParameter("query"),
+        ),
     ],
     # No archived queries found for:
     "netspeak": [
-        QuerySource("netspeak.org/?q=", FragmentParameter("q")),
+        QuerySource(
+            "netspeak.org/?q=",
+            FragmentParameter("q"),
+        ),
     ],
     # TODO Add more search engines
     #  - https://en.wikipedia.org/wiki/List_of_search_engines).
