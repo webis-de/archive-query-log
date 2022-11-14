09d4e605bd7189b64fa701dba6dc14787a4d119e
Sebastian Schmidt <s.schmidt@uni-leipzig.de>
2022-11-14 15:01:53 +0100

    Sorted config.py alphabetically

--- internet_archive_query_log/config.py
+++ internet_archive_query_log/config.py
@@ -7,6 +7,113 @@ from internet_archive_query_log.sites import WIKIPEDIA_SITES, AMAZON_SITES, \
     EBAY_SITES, STACKEXCHANGE_SITES
 
 SOURCES: Mapping[str, Sequence[Source]] = {
+    "amazon": [
+        Source(
+            url_prefix=f"{site}/s?",
+            query_parser=QueryParameter("k"),
+            serp_parsers=[],
+        )
+        for site in AMAZON_SITES
+    ],
+    "argsme": [
+        Source(
+            url_prefix="args.me/?query=",
+            query_parser=QueryParameter("query"),
+            serp_parsers=[],
+        ),
+    ],
+    "ask": [
+        Source(
+            url_prefix="ask.com/web?",
+            query_parser=QueryParameter("q"),
+            serp_parsers=[]
+        )
+    ],
+    "bing": [
+        # TODO Add all global pages.
+        Source(
+            url_prefix="bing.com/search?",
+            query_parser=QueryParameter("q"),
+            serp_parsers=[BingSerpParser()],
+        ),
+    ],
+    "chatnoir": [
+        Source(
+            url_prefix="chatnoir.eu/?",
+            query_parser=QueryParameter("q"),
+            serp_parsers=[],
+        ),
+    ],
+    "dblp": [
+        Source(
+            url_prefix="dblp.org/search?",
+            query_parser=QueryParameter("q"),
+            serp_parsers=[],
+        ),
+        Source(
+            url_prefix="dblp.uni-trier.de/search?",
+            query_parser=QueryParameter("q"),
+            serp_parsers=[],
+        ),
+    ],
+    "duckduckgo": [
+        Source(
+            url_prefix="duckduckgo.com/?",
+            query_parser=QueryParameter("q"),
+            serp_parsers=[],
+        ),
+    ],
+    "ebay": [
+                Source(
+                    url_prefix=f"{site}/?",
+                    query_parser=QueryParameter("_nkw"),
+                    serp_parsers=[],
+                )
+                for site in EBAY_SITES
+            ] + [
+                Source(
+                    url_prefix=f"{site}/sch/i.html?",
+                    query_parser=QueryParameter("_nkw"),
+                    serp_parsers=[],
+                )
+                for site in EBAY_SITES
+            ],
+    "ecosia": [
+        Source(
+            url_prefix="ecosia.org/search?",
+            query_parser=QueryParameter("q"),
+            serp_parsers=[],
+        ),
+        Source(
+            url_prefix="ecosia.org/news?",
+            query_parser=QueryParameter("q"),
+            serp_parsers=[],
+        ),
+        Source(
+            url_prefix="ecosia.org/images?",
+            query_parser=QueryParameter("q"),
+            serp_parsers=[],
+        ),
+        Source(
+            url_prefix="ecosia.org/videos?",
+            query_parser=QueryParameter("q"),
+            serp_parsers=[],
+        ),
+    ],
+    "facebook": [
+        Source(
+            url_prefix="facebook.com/search?",
+            query_parser=QueryParameter("q"),
+            serp_parsers=[],
+        ),
+    ],
+    "github": [
+        Source(
+            url_prefix="github.com/search?",
+            query_parser=QueryParameter("q"),
+            serp_parsers=[],
+        ),
+    ],
     "google": [
         # TODO Test
         # TODO Add all global pages
@@ -59,35 +166,61 @@ SOURCES: Mapping[str, Sequence[Source]] = {
             serp_parsers=[],
         ),
     ],
-    "bing": [
-        # TODO Add all global pages.
+    "google-scholar": [
         Source(
-            url_prefix="bing.com/search?",
+            url_prefix="scholar.google.com/scholar?",
             query_parser=QueryParameter("q"),
-            serp_parsers=[BingSerpParser()],
+            serp_parsers=[],
         ),
     ],
-    "ecosia": [
+    "internet-archive": [
         Source(
-            url_prefix="ecosia.org/search?",
+            url_prefix="archive.org/search.php?",
+            query_parser=QueryParameter("query"),
+            serp_parsers=[],
+        ),
+    ],
+    "lastfm": [
+        Source(
+            url_prefix="last.fm/search?",
             query_parser=QueryParameter("q"),
             serp_parsers=[],
         ),
+    ],
+    "pubmed": [
         Source(
-            url_prefix="ecosia.org/news?",
-            query_parser=QueryParameter("q"),
+            url_prefix="pubmed.gov/?",
+            query_parser=QueryParameter("term"),
             serp_parsers=[],
         ),
         Source(
-            url_prefix="ecosia.org/images?",
-            query_parser=QueryParameter("q"),
+            url_prefix="pubmed.ncbi.nlm.nih.gov/?",
+            query_parser=QueryParameter("term"),
             serp_parsers=[],
         ),
+    ],
+    "stackexchange": [
         Source(
-            url_prefix="ecosia.org/videos?",
+            url_prefix=f"{site}/search?",
+            query_parser=QueryParameter("q"),
+            serp_parsers=[],
+        )
+        for site in STACKEXCHANGE_SITES
+    ],
+    "twitter": [
+        Source(
+            url_prefix="twitter.com/search?",
             query_parser=QueryParameter("q"),
             serp_parsers=[],
         ),
+    ],
+    "wikipedia": [
+        Source(
+            url_prefix=f"{site}/w/index.php?",
+            query_parser=QueryParameter("search"),
+            serp_parsers=[],
+        )
+        for site in WIKIPEDIA_SITES
     ],
     "yahoo": [
         # TODO Test
@@ -108,132 +241,6 @@ SOURCES: Mapping[str, Sequence[Source]] = {
             serp_parsers=[],
         ),
     ],
-    "duckduckgo": [
-        Source(
-            url_prefix="duckduckgo.com/?",
-            query_parser=QueryParameter("q"),
-            serp_parsers=[],
-        ),
-    ],
-    "internet-archive": [
-        Source(
-            url_prefix="archive.org/search.php?",
-            query_parser=QueryParameter("query"),
-            serp_parsers=[],
-        ),
-    ],
-    "wikipedia": [
-        Source(
-            url_prefix=f"{site}/w/index.php?",
-            query_parser=QueryParameter("search"),
-            serp_parsers=[],
-        )
-        for site in WIKIPEDIA_SITES
-    ],
-    "amazon": [
-        Source(
-            url_prefix=f"{site}/s?",
-            query_parser=QueryParameter("k"),
-            serp_parsers=[],
-        )
-        for site in AMAZON_SITES
-    ],
-    "ebay": [
-                Source(
-                    url_prefix=f"{site}/?",
-                    query_parser=QueryParameter("_nkw"),
-                    serp_parsers=[],
-                )
-                for site in EBAY_SITES
-            ] + [
-                Source(
-                    url_prefix=f"{site}/sch/i.html?",
-                    query_parser=QueryParameter("_nkw"),
-                    serp_parsers=[],
-                )
-                for site in EBAY_SITES
-            ],
-    "lastfm": [
-        Source(
-            url_prefix="last.fm/search?",
-            query_parser=QueryParameter("q"),
-            serp_parsers=[],
-        ),
-    ],
-    "twitter": [
-        Source(
-            url_prefix="twitter.com/search?",
-            query_parser=QueryParameter("q"),
-            serp_parsers=[],
-        ),
-    ],
-    "facebook": [
-        Source(
-            url_prefix="facebook.com/search?",
-            query_parser=QueryParameter("q"),
-            serp_parsers=[],
-        ),
-    ],
-    "pubmed": [
-        Source(
-            url_prefix="pubmed.gov/?",
-            query_parser=QueryParameter("term"),
-            serp_parsers=[],
-        ),
-        Source(
-            url_prefix="pubmed.ncbi.nlm.nih.gov/?",
-            query_parser=QueryParameter("term"),
-            serp_parsers=[],
-        ),
-    ],
-    "google-scholar": [
-        Source(
-            url_prefix="scholar.google.com/scholar?",
-            query_parser=QueryParameter("q"),
-            serp_parsers=[],
-        ),
-    ],
-    "dblp": [
-        Source(
-            url_prefix="dblp.org/search?",
-            query_parser=QueryParameter("q"),
-            serp_parsers=[],
-        ),
-        Source(
-            url_prefix="dblp.uni-trier.de/search?",
-            query_parser=QueryParameter("q"),
-            serp_parsers=[],
-        ),
-    ],
-    "github": [
-        Source(
-            url_prefix="github.com/search?",
-            query_parser=QueryParameter("q"),
-            serp_parsers=[],
-        ),
-    ],
-    "stackexchange": [
-        Source(
-            url_prefix=f"{site}/search?",
-            query_parser=QueryParameter("q"),
-            serp_parsers=[],
-        )
-        for site in STACKEXCHANGE_SITES
-    ],
-    "chatnoir": [
-        Source(
-            url_prefix="chatnoir.eu/?",
-            query_parser=QueryParameter("q"),
-            serp_parsers=[],
-        ),
-    ],
-    "argsme": [
-        Source(
-            url_prefix="args.me/?query=",
-            query_parser=QueryParameter("query"),
-            serp_parsers=[],
-        ),
-    ],
     "yandex": [
         Source(
             url_prefix="yandex.com/search/?",
@@ -248,13 +255,6 @@ SOURCES: Mapping[str, Sequence[Source]] = {
         Source(
             url_prefix="yandex.com/video/search/?",
             query_parser=QueryParameter("text"),
-            serp_parsers=[]
-        )
-    ],
-    "ask": [
-        Source(
-            url_prefix="ask.com/web?",
-            query_parser=QueryParameter("q"),
             serp_parsers=[]
         )
     ],
