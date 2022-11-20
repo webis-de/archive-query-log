50ac122e995f5ee9d3a44a025de9d1a1b14e80a3
Jan Heinrich Reimer <jan.reimer@student.uni-halle.de>
2022-11-20 22:40:06 +0100

    Improve downloader

--- web_archive_query_log/util/download.py
+++ web_archive_query_log/util/download.py
@@ -2,7 +2,7 @@ from asyncio import ensure_future, gather, sleep
 from contextlib import asynccontextmanager
 from pathlib import Path
 from random import random
-from typing import Iterable, Mapping
+from typing import Iterable, Mapping, Callable
 
 from aiohttp import TCPConnector, ClientSession, ClientTimeout, \
     ClientResponseError, ClientConnectorError, ServerTimeoutError, \
@@ -12,23 +12,24 @@ from tqdm.auto import tqdm
 
 from web_archive_query_log.model import ArchivedUrl
 
-_IO_CHUNK_SIZE = 64 * 1000 * 1000  # 64MB
+_MAX_CONNECTIONS_PER_HOST = 10
+_MAX_BYTES_PER_SECOND = 1 * 1000 * 1000  # 1MB
 
 
 class WebArchiveRawDownloader:
 
     @asynccontextmanager
     async def _session(self) -> ClientSession:
-        # The Wayback Machine doesn't seem to support more than 20
+        # The Wayback Machine doesn't seem to support more than 10
         # parallel connections from the same IP.
         connector = TCPConnector(
-            limit=100,
-            limit_per_host=20,
+            limit_per_host=_MAX_CONNECTIONS_PER_HOST,
         )
         # Graceful timeout as the Wayback Machine is sometimes very slow.
         timeout = ClientTimeout(
-            total=10 * 60,
-            connect=5 * 60,
+            total=15 * 60,
+            connect=5 * 60,  # Setting up a connection is especially slow.
+            sock_read=5 * 60,
         )
         async with ClientSession(
                 connector=connector,
@@ -41,7 +42,7 @@ class WebArchiveRawDownloader:
         retry_options = JitterRetry(
             attempts=10,
             start_timeout=1,  # 1 second
-            max_timeout=5 * 60,  # 3 minutes
+            max_timeout=15 * 60,  # 15 minutes
             statuses={502, 503, 504},  # server errors
             exceptions={
                 ClientConnectorError,
@@ -58,8 +59,9 @@ class WebArchiveRawDownloader:
 
     async def download(
             self,
-            download_directory_path: Path,
             archived_urls: Iterable[ArchivedUrl],
+            download_directory_path: Path,
+            file_name: Callable[[ArchivedUrl], str],
     ) -> Mapping[ArchivedUrl, Path]:
         """
         Download the original HTML of a collection of archived URLs
@@ -69,9 +71,11 @@ class WebArchiveRawDownloader:
         The downloader will retry requests with a backoff and will continue
         downloading URLs even if some URLs fail.
 
+        :param archived_urls: The archived URLs to download.
         :param download_directory_path: Path to the directory in which the
          downloaded HTML files should be stored.
-        :param archived_urls: The archived URLs to download.
+        :param file_name: Function for specifying each URL's file name
+         in the download directory.
         :return: A mapping of the successfully downloaded URLs to their
          corresponding download file paths. Failed downloads will not appear
          in the mapping.
@@ -85,10 +89,11 @@ class WebArchiveRawDownloader:
         async with self._client() as client:
             responses = await gather(*(
                 ensure_future(self._download_single_progress(
-                    download_directory_path,
-                    archived_url,
-                    client,
-                    progress,
+                    archived_url=archived_url,
+                    download_directory_path=download_directory_path,
+                    file_name=file_name,
+                    client=client,
+                    progress=progress,
                 ))
                 for archived_url in archived_urls
             ))
@@ -100,8 +105,9 @@ class WebArchiveRawDownloader:
 
     async def download_single(
             self,
-            download_directory_path: Path,
             archived_url: ArchivedUrl,
+            download_directory_path: Path,
+            file_name: Callable[[ArchivedUrl], str],
     ) -> Path | None:
         """
         Download the original HTML of a single archived URL
@@ -110,53 +116,61 @@ class WebArchiveRawDownloader:
 
         The downloader will retry requests with a backoff.
 
+        :param archived_url: The archived URL to download.
         :param download_directory_path: Path to the directory in which the
          downloaded HTML file should be stored.
-        :param archived_url: The archived URL to download.
+        :param file_name: Function for specifying each URL's file name
+         in the download directory.
         :return: The download file path if the request was successful,
          and None otherwise.
         """
         async with self._client() as client:
             return await self._download_single(
-                download_directory_path,
-                archived_url,
-                client,
+                archived_url=archived_url,
+                download_directory_path=download_directory_path,
+                file_name=file_name,
+                client=client,
             )
 
     @staticmethod
     async def _download_single(
-            download_directory_path: Path,
             archived_url: ArchivedUrl,
+            download_directory_path: Path,
+            file_name: Callable[[ArchivedUrl], str],
             client: RetryClient,
     ) -> Path | None:
-        file_name = f"{archived_url.url_domain}_{archived_url.url_md5}" \
-                    f"_{archived_url.timestamp}.html"
-        file_path = download_directory_path / file_name
+        file_path = download_directory_path / file_name(archived_url)
         if file_path.exists():
             return file_path
+        url = archived_url.raw_archive_url
+        await sleep(1.0 * random())
+        print(f"Request {url}")
         try:
-            async with client.get(archived_url.raw_archive_url) as response:
+            async with client.get(url) as response:
                 with file_path.open("wb") as file:
-                    async for chunk in response.content.iter_chunked(
-                            _IO_CHUNK_SIZE
-                    ):
-                        file.write(chunk)
+                    async for data, _ in response.content.iter_chunks():
+                        file.write(data)
                 return file_path
-        except ClientResponseError as e:
+        except ClientResponseError:
+            file_path.unlink(missing_ok=True)
             return None
+        except BaseException as e:
+            file_path.unlink(missing_ok=True)
+            raise e
 
     @staticmethod
     async def _download_single_progress(
-            download_directory_path: Path,
             archived_url: ArchivedUrl,
+            download_directory_path: Path,
+            file_name: Callable[[ArchivedUrl], str],
             client: RetryClient,
             progress: tqdm,
     ) -> Path | None:
-        await sleep(random())
         res = await WebArchiveRawDownloader._download_single(
-            download_directory_path,
-            archived_url,
-            client,
+            archived_url=archived_url,
+            download_directory_path=download_directory_path,
+            file_name=file_name,
+            client=client,
         )
         progress.update(1)
         return res
