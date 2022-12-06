ee25b10e6bf61e83f925c814c584c7fb175c558e
Jan Heinrich Reimer <jan.reimer@student.uni-halle.de>
2022-12-06 19:48:35 +0100

    Add serp parser class

--- web_archive_query_log/results/parse.py
+++ web_archive_query_log/results/parse.py
@@ -1,10 +1,16 @@
+from dataclasses import dataclass
+from gzip import GzipFile
+from io import TextIOWrapper
+from pathlib import Path
 from typing import Sequence, Iterator
 
 from bleach import clean
 from bs4 import Tag, BeautifulSoup
+from tqdm.auto import tqdm
 
+from web_archive_query_log.download.iterable import ArchivedSerpContents
 from web_archive_query_log.model import ArchivedSerpContent, SearchResult, \
-    ResultsParser
+    ResultsParser, InterpretedQueryParser, ArchivedSerp
 
 
 class BingResultsParser(ResultsParser):
@@ -51,3 +57,65 @@ class BingResultsParser(ResultsParser):
             # bing.*/*
             return None
         return list(self._parse_serp_iter(content.content, content.encoding))
+
+
+@dataclass(frozen=True)
+class ArchivedSerpsParser:
+    results_parsers: Sequence[ResultsParser]
+    result_query_parsers: Sequence[InterpretedQueryParser]
+    verbose: bool = False
+
+    def parse(self, input_path: Path, output_path: Path) -> None:
+        archived_serp_contents = ArchivedSerpContents(input_path)
+        if self.verbose:
+            archived_serp_contents = tqdm(
+                archived_serp_contents,
+                desc="Parse SERP WARC records",
+                unit="record",
+            )
+        archived_serps = (
+            self._parse_single(archived_serp_content)
+            for archived_serp_content in archived_serp_contents
+        )
+        archived_serps = (
+            archived_serp
+            for archived_serp in archived_serps
+            if archived_serp is not None
+        )
+        output_schema = ArchivedSerp.schema()
+        # noinspection PyTypeChecker
+        with output_path.open("wb") as file, \
+                GzipFile(fileobj=file, mode="wb") as gzip_file, \
+                TextIOWrapper(gzip_file) as text_file:
+            for archived_serp_url in archived_serps:
+                text_file.write(output_schema.dumps(archived_serp_url))
+                text_file.write("\n")
+
+    def _parse_single(
+            self,
+            archived_serp_content: ArchivedSerpContent
+    ) -> ArchivedSerp | None:
+        results: Sequence[SearchResult] | None = None
+        for parser in self.results_parsers:
+            results = parser.parse(archived_serp_content)
+            if results is not None:
+                break
+
+        if results is None:
+            return None
+
+        interpreted_query: str | None = None
+        for parser in self.result_query_parsers:
+            interpreted_query = parser.parse(archived_serp_content)
+            if interpreted_query is not None:
+                break
+
+        return ArchivedSerp(
+            url=archived_serp_content.url,
+            timestamp=archived_serp_content.timestamp,
+            query=archived_serp_content.query,
+            page=archived_serp_content.page,
+            offset=archived_serp_content.offset,
+            interpreted_query=interpreted_query,
+            results=results,
+        )
