5c77ca70550f5c70f192e6b12117da1ac135add9
Jan Heinrich Reimer <jan.reimer@student.uni-halle.de>
2022-12-06 01:31:25 +0100

    Update query CLI

--- web_archive_query_log/cli/queries.py
+++ web_archive_query_log/cli/queries.py
@@ -1,13 +1,13 @@
 from pathlib import Path
-from typing import Optional
 
-from click import Choice, argument, option
+from click import option, Choice, argument
+from tqdm.auto import tqdm
 
-from web_archive_query_log import CDX_API_URL, DATA_DIRECTORY_PATH
+from web_archive_query_log import DATA_DIRECTORY_PATH
+from web_archive_query_log.config import SERVICES
 from web_archive_query_log.cli import main
-from web_archive_query_log.cli.util import URL, PathParam
-from web_archive_query_log.config import SOURCES
-from web_archive_query_log.queries import InternetArchiveQueries
+from web_archive_query_log.cli.util import PathParam
+from web_archive_query_log.queries.parse import ArchivedSerpUrlsParser
 
 
 @main.group("queries")
@@ -15,87 +15,45 @@ def queries():
     pass
 
 
-@queries.command("fetch")
+@queries.command("parse-service")
 @option(
-    "-d", "--data-dir",
+    "-d", "--data-directory", "--data-directory-path",
     type=PathParam(
         exists=True,
         file_okay=False,
         dir_okay=True,
         writable=True,
-        readable=True,
+        readable=False,
         resolve_path=True,
         path_type=Path,
     ),
     default=DATA_DIRECTORY_PATH
 )
-@option(
-    "-u", "--api-url", "--cdx-api-url",
-    type=URL,
-    default=CDX_API_URL,
-)
 @argument(
-    "search-engine",
-    type=Choice(sorted(SOURCES.keys()), case_sensitive=False),
+    "service_name",
+    type=Choice(sorted(SERVICES.keys())),
+    required=True,
 )
-def fetch(
-        search_engine: str,
-        data_dir: Path,
-        api_url: str,
+def fetch_service(
+        data_directory: Path,
+        service_name: str,
 ) -> None:
-    config = SOURCES[search_engine]
-    for source in config:
-        queries = InternetArchiveQueries(
-            url_prefix=source.url_prefix,
-            parser=source.query_parser,
-            data_directory_path=data_dir,
-            cdx_api_url=api_url,
+    from web_archive_query_log.config import SERVICES
+    service = SERVICES[service_name]
+    service_dir = data_directory / service.name
+    parser = ArchivedSerpUrlsParser(
+        query_parsers=service.query_parsers,
+        page_number_parsers=service.page_num_parsers,
+        verbose=True,
+    )
+    service_domain_dirs = list(service_dir.glob("*"))
+    service_domain_dirs = tqdm(
+        service_domain_dirs,
+        desc=f"Parse queries",
+        unit="domains",
+    )
+    for service_domain_dir in service_domain_dirs:
+        parser.parse(
+            input_path=service_domain_dir / "urls.jsonl",
+            output_path=service_domain_dir / "serp-urls.jsonl",
         )
-        queries.fetch()
-
-
-@queries.command("num-pages")
-@option(
-    "-d", "--data-dir",
-    type=PathParam(
-        exists=True,
-        file_okay=False,
-        dir_okay=True,
-        writable=True,
-        readable=True,
-        resolve_path=True,
-        path_type=Path,
-    ),
-    default=DATA_DIRECTORY_PATH
-)
-@option(
-    "-u", "--api-url", "--cdx-api-url",
-    type=URL,
-    default=CDX_API_URL,
-)
-@argument(
-    "search-engine",
-    type=Choice(sorted(SOURCES.keys()), case_sensitive=False),
-    required=False,
-)
-def num_pages(
-        api_url: str,
-        data_dir: Path,
-        search_engine: Optional[str]
-) -> None:
-    configs = SOURCES.values() \
-        if search_engine is None \
-        else (SOURCES[search_engine],)
-    total_pages = 0
-    for config in configs:
-        for source in config:
-            queries = InternetArchiveQueries(
-                url_prefix=source.url_prefix,
-                parser=source.query_parser,
-                data_directory_path=data_dir,
-                cdx_api_url=api_url,
-            )
-            pages = queries.num_pages
-            print(f"{source.url_prefix}: {pages} pages")
-            total_pages += pages
-    print(f"total: {total_pages} pages")
