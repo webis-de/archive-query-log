e117e70d470f9c0d5d56cf286e74a7ddfe3a5fde
Jan Heinrich Reimer <jan.reimer@student.uni-halle.de>
2022-12-06 02:18:29 +0100

    Add download service CLI

--- web_archive_query_log/cli/download.py
+++ web_archive_query_log/cli/download.py
@@ -1,9 +1,15 @@
 from asyncio import run
 from pathlib import Path
 
-from click import argument, Path as PathParam
+from click import argument, Path as PathParam, option, Choice
+from tqdm.auto import tqdm
 
+from web_archive_query_log import DATA_DIRECTORY_PATH
+from web_archive_query_log.config import SERVICES
 from web_archive_query_log.cli import main
+from web_archive_query_log.download.warc import WebArchiveWarcDownloader
+from web_archive_query_log.queries.iterable import ArchivedSerpUrls
+from web_archive_query_log.urls.iterable import ArchivedUrls
 
 
 @main.group("download")
@@ -37,10 +43,48 @@ def download():
     ),
 )
 def warc(download_dir: Path, urls_file: Path) -> None:
-    from web_archive_query_log.download.warc import WebArchiveWarcDownloader
-    from web_archive_query_log.urls.iterable import ArchivedUrls
-    downloader = WebArchiveWarcDownloader(
-        ArchivedUrls(urls_file),
+    downloader = WebArchiveWarcDownloader()
+    run(downloader.download(
         download_dir,
+        ArchivedUrls(urls_file),
+    ))
+
+
+@download.command("download-service")
+@option(
+    "-d", "--data-directory", "--data-directory-path",
+    type=PathParam(
+        exists=True,
+        file_okay=False,
+        dir_okay=True,
+        writable=True,
+        readable=False,
+        resolve_path=True,
+        path_type=Path,
+    ),
+    default=DATA_DIRECTORY_PATH
+)
+@argument(
+    "service_name",
+    type=Choice(sorted(SERVICES.keys())),
+    required=True,
+)
+def fetch_service(
+        data_directory: Path,
+        service_name: str,
+) -> None:
+    service = SERVICES[service_name]
+    service_dir = data_directory / service.name
+    downloader = WebArchiveWarcDownloader()
+    domains = service.domains
+    domains = tqdm(
+        domains,
+        desc=f"Download SERPs",
+        unit="domain",
     )
-    run(downloader.download())
+    for domain in domains:
+        domain_dir = service_dir / domain
+        run(downloader.download(
+            domain_dir / "serps",
+            ArchivedSerpUrls(domain_dir / "serp-urls.jsonl.gz"),
+        ))
