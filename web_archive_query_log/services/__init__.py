f9c00883c365cd1832f7e5f99c3b087962d51f99
Jan Heinrich Reimer <jan.reimer@student.uni-halle.de>
2022-12-16 23:43:05 +0100

    Check services fo duplicates

--- web_archive_query_log/services/__init__.py
+++ web_archive_query_log/services/__init__.py
@@ -9,7 +9,16 @@ from web_archive_query_log.model import Service
 def read_services(path: Path) -> Mapping[str, Service]:
     with path.open("r") as file:
         services_dict = safe_load(file)
-        return {
-            service.name: service
+        services = [
+            (service.name, service)
             for service in Service.schema().load(services_dict, many=True)
+        ]
+        service_names = set()
+        for name, service in services:
+            if name in service_names:
+                raise ValueError(f"Duplicate service name {name}")
+            service_names.add(name)
+        return {
+            name: service
+            for name, service in services
         }
