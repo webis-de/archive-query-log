9e3683ea793931c9fbde93938fc9923e5a4650f4
Jan Heinrich Reimer <jan.reimer@student.uni-halle.de>
2022-12-19 00:24:54 +0100

    Move parser

--- web_archive_query_log/results/parse.py
+++ web_archive_query_log/results/parse.py
@@ -2,66 +2,15 @@ from dataclasses import dataclass
 from gzip import GzipFile
 from io import TextIOWrapper
 from pathlib import Path
-from typing import Sequence, Iterator, Pattern, NamedTuple
+from typing import Sequence, NamedTuple
 from urllib.parse import quote
 
-from bleach import clean
-from bs4 import Tag, BeautifulSoup
 from tqdm.auto import tqdm
 
 from web_archive_query_log.download.iterable import ArchivedRawSerps
 from web_archive_query_log.model import ArchivedRawSerp, ArchivedSerpResult, \
     ResultsParser, InterpretedQueryParser, ArchivedParsedSerp, Service
 
-
-@dataclass(frozen=True)
-class BingResultsParser(ResultsParser):
-    url_pattern: Pattern[str]
-
-    @staticmethod
-    def _clean_html(tag: Tag) -> str:
-        return clean(
-            tag.decode_contents(),
-            tags=["strong"],
-            attributes=[],
-            protocols=[],
-            strip=True,
-            strip_comments=True,
-        ).strip()
-
-    def _parse_serp_iter(
-            self,
-            content: bytes,
-            encoding: str,
-    ) -> Iterator[ArchivedSerpResult]:
-        soup = BeautifulSoup(content, "html.parser", from_encoding=encoding)
-        results: Tag = soup.find("ol", id="b_results")
-        if results is None:
-            return
-        result: Tag
-        for result in results.find_all("li", class_="b_algo"):
-            title: Tag = result.find("h2")
-            caption: Tag | None = result.find("p")
-            yield ArchivedSerpResult(
-                url=title.find("a").attrs["href"],
-                title=self._clean_html(title),
-                snippet=(
-                    self._clean_html(caption)
-                    if caption is not None else ""
-                )
-            )
-
-    def _parse_results(
-            self,
-            content: ArchivedRawSerp,
-    ) -> Sequence[ArchivedSerpResult] | None:
-        if self.url_pattern.search(content.url) is None:
-            return None
-        domain_parts = content.split_url.netloc.split(".")
-        if "bing" not in domain_parts:
-            # bing.*/*
-            return None
-        return list(self._parse_serp_iter(content.content, content.encoding))
 
 class _CdxPage(NamedTuple):
     input_path: Path
