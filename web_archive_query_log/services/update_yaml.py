e30d8aa055f6763758da7842eaad2653b8b4b4e8
Sebastian Schmidt <s.schmidt@uni-leipzig.de>
2022-12-20 10:12:06 +0100

    Updated yaml file based on difference between update and overwrite script. Diff obtained using the dyff tool

--- web_archive_query_log/services/update_yaml.py
+++ web_archive_query_log/services/update_yaml.py
@@ -5,9 +5,15 @@ from web_archive_query_log.cli.external import load_services, load_domains, serv
     load_query_parsers, query_parser, load_page_offset_parsers, page_offset_parser_series
 from web_archive_query_log import DATA_DIRECTORY_PATH
 
-services_file = DATA_DIRECTORY_PATH / "selected-services.yaml"
+services_file = DATA_DIRECTORY_PATH / "selected-services_overwrite.yaml"
 
 def get_spreadsheet_data(first_service="google", last_service="hrblock") -> DataFrame:
+    """
+    Get parser information from the Google spreadsheet as a dataframe
+    :param first_service:   First service to obtain data for (including)
+    :param last_service:    Last service to obtain data for (including)
+    :return:                Dataframe with parser information for each service
+    """
     services = load_services()
     idx_first = services["name"].ne(first_service).idxmin()
     idx_last = services["name"].ne(last_service).idxmin()
@@ -30,7 +36,7 @@ def get_spreadsheet_data(first_service="google", last_service="hrblock") -> Data
             query_parser(row)
             for _, row in
             query_parsers[
-                query_parsers["name"].str.endswith(service["name"])
+                query_parsers["name"].str.fullmatch(service["name"])
             ].iterrows()
         ), key=lambda qp: str(qp["url_pattern"]))
         for _, service in services.iterrows()
@@ -47,11 +53,18 @@ def get_spreadsheet_data(first_service="google", last_service="hrblock") -> Data
     return services
 
 
-def update_yaml_file(first_service="google", last_service="hrblock"):
+def update_yaml_file(first_service="google", last_service="hrblock", overwrite=False):
+    """
+    Update the local yaml file with the data from the Google spreadsheet
+    :param first_service:   First service to update (including)
+    :param last_service:    Last service to update (including)
+    :param overwrite:       False: Only add parsers for service that don't have one
+                            True:   Overwrite the yaml entries using the spreadsheet
+    """
     services = get_spreadsheet_data(first_service=first_service, last_service=last_service)
     with open(services_file, "r") as stream:
         yaml_list = yaml.safe_load(stream)
-
+    update_func = overwrite_parsers if overwrite else update_empty_parsers
     i = 0
     start_update = False
     while not start_update:
@@ -64,15 +77,7 @@ def update_yaml_file(first_service="google", last_service="hrblock"):
     while True:
         elem = yaml_list[i]
         name = elem["name"]
-        offset_parsers = elem["offset_parsers"]
-        page_parsers = elem["page_parsers"]
-        query_parsers = elem["query_parsers"]
-
-        # If no parsers are set, update the element using the services df
-        if len(offset_parsers) + len(page_parsers) < 1:
-            yaml_list[i] = set_page_offset_parsers(service_elem=elem, services=services)
-        if len(query_parsers) == 0:
-            set_query_parsers(service_elem=elem, services=services)
+        update_func(service_elem=elem, services=services)
         i += 1
         if name == last_service:
             break
@@ -81,17 +86,32 @@ def update_yaml_file(first_service="google", last_service="hrblock"):
         yaml.dump(yaml_list, stream=file, sort_keys=False)
 
 
+def update_empty_parsers(service_elem: dict, services: DataFrame):
+    offset_parsers = service_elem["offset_parsers"]
+    page_parsers = service_elem["page_parsers"]
+    query_parsers = service_elem["query_parsers"]
+
+    # If no parsers are set, update the element using the services df
+    if len(offset_parsers) + len(page_parsers) < 1:
+        set_page_offset_parsers(service_elem=service_elem, services=services)
+    if len(query_parsers) == 0:
+        set_query_parsers(service_elem=service_elem, services=services)
+
+
+def overwrite_parsers(service_elem: dict, services: DataFrame):
+   set_page_offset_parsers(service_elem=service_elem, services=services)
+   set_query_parsers(service_elem=service_elem, services=services)
+
+
 
 def set_page_offset_parsers(service_elem: dict, services: DataFrame) -> Mapping:
     name = service_elem["name"]
     row = services.loc[services["name"] == name, :]
     service_elem.update({"page_parsers": row["page_parsers"].values[0],
                          "offset_parsers": row["offset_parsers"].values[0]})
-    return service_elem
 
 def set_query_parsers(service_elem: dict, services: DataFrame) -> Mapping:
     name = service_elem["name"]
     row = services.loc[services["name"] == name, :]
     service_elem.update({"query_parsers": row["query_parsers"].values[0]})
-    return service_elem
 
