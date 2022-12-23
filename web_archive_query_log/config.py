7f9411849cc8135e2693aec5603535cc6858ed8c
Jan Heinrich Reimer <jan.reimer@student.uni-halle.de>
2022-12-23 14:44:57 +0100

    Move and simplify test

--- web_archive_query_log/config.py
+++ web_archive_query_log/config.py
@@ -5,5 +5,5 @@ from web_archive_query_log.model import Service
 from web_archive_query_log.services import read_services
 
 # Load all services that have parsers and create the services for them.
-_SERVICES_PATH = DATA_DIRECTORY_PATH / "selected-services.yaml"
-SERVICES: Mapping[str, Service] = read_services(_SERVICES_PATH)
+SERVICES_PATH = DATA_DIRECTORY_PATH / "selected-services.yaml"
+SERVICES: Mapping[str, Service] = read_services(SERVICES_PATH)
--- web_archive_query_log/results/test/test_all_services_can_be_parsed.py
+++
@@ -1,7 +0,0 @@
-from unittest import TestCase
-from web_archive_query_log.services import read_services
-from web_archive_query_log.config import _SERVICES_PATH
-
-class TestAllServicesCanBeParsed(TestCase):
-    def test_services_can_be_parsed(self):
-        self.assertIsNotNone(read_services(_SERVICES_PATH, ignore_parsing_errors=False))
