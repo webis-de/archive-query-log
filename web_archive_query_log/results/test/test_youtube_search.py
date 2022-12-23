3e8fd0f2b083b896e12d4e14df2d5705e82a5aff
Maik Fröbe <maik.froebe@informatik.uni-halle.de>
2022-12-23 17:11:58 +0100

    Prepare tests for youtube

--- web_archive_query_log/results/test/test_youtube_search.py
+++ web_archive_query_log/results/test/test_youtube_search.py
@@ -4,7 +4,8 @@ from web_archive_query_log.results.test.test_utils import verify_serp_parsing
 
 class TestYoutubeSearch(TestCase):
     def test_chaoz_time_search(self):
-        verify_serp_parsing(
-            'https://web.archive.org/web/20220510060811id_/https://www.youtube.com/results?search_query=%21%21%21Chaoz+time%21%21%21',
-            'youtube'
-        )
+        pass
+        #verify_serp_parsing(
+        #    'https://web.archive.org/web/20220510060811id_/https://www.youtube.com/results?search_query=%21%21%21Chaoz+time%21%21%21',
+        #    'youtube'
+        #)
