33c211160dbea922bd77cb2b88fd1e4e38c8593c
Maik Fröbe <maik.froebe@informatik.uni-halle.de>
2022-12-23 11:29:34 +0100

    Add more tests for facebook search

--- web_archive_query_log/results/test/test_facebook_search.py
+++ web_archive_query_log/results/test/test_facebook_search.py
@@ -8,3 +8,9 @@ class TestFacebookSearch(TestCase):
             'https://web.archive.org/web/20140917041101id_/https://www.facebook.com/search.php?q=%22Jam+of+the+Day&init=quick&tas=0.8517628074453497&search_first_focus=1302687872720',
             'facebook'
         )
+
+    def test_victoria_pynchon_search(self):
+        verify_serp_parsing(
+            'https://web.archive.org/web/20110110162620id_/http://www.facebook.com/search.php?q=%22Victoria+Pynchon%22&init=q',
+            'facebook'
+        )
