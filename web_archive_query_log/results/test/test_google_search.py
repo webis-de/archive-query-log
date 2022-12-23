2a2fba2ffda48a2b9a7ba5457291bafa034de5bb
Maik Fröbe <maik.froebe@informatik.uni-halle.de>
2022-12-23 13:53:40 +0100

    Add more tests for google

--- web_archive_query_log/results/test/test_google_search.py
+++ web_archive_query_log/results/test/test_google_search.py
@@ -14,9 +14,20 @@ class TestGoogleSearch(TestCase):
             'https://web.archive.org/web/20210227012632id_/http://www.google.com/search?hl=en&lr=&ie=ISO-8859-1&q=%22+Lenin%22',
             'google'
         )
+
+    def test_august_search(self):
+        verify_serp_parsing(
+            'https://web.archive.org/web/20210225074449id_/http://www.google.com/search?hl=en&q=%22%22+%22Agust&iacute;n',
+            'google'
+        )
+
+    def test_cortisol_search(self):
+        verify_serp_parsing(
+            'https://web.archive.org/web/20210609042746id_/http://www.google.com/search?hl=en&lr=&ie=ISO-8859-1&safe=off&q=++++%22+Cortisol+test%22',
+            'google'
+        )
 #
-#https://web.archive.org/web/20210225074449id_/http://www.google.com/search?hl=en&q=%22%22+%22Agust&iacute;n
-# https://web.archive.org/web/20210609042746id_/http://www.google.com/search?hl=en&lr=&ie=ISO-8859-1&safe=off&q=++++%22+Cortisol+test%22
+#
 #https://web.archive.org/web/20210330143639id_/http://www.google.com/search?hl=en&ie=ISO-8859-1&q=%22+coxsackie+virus%22_
 #https://web.archive.org/web/20210330143719id_/http://www.google.com/search?hl=en&source=hp&ie=ISO-8859-1&q=%22+homemade+l
 #https://web.archive.org/web/20210330143611id_/http://www.google.com/search?hl=en&ie=ISO-8859-1&q=%22+dove%22+%22soap%22+%22
