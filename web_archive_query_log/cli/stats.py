1c8f86774aa85419c285829e07e83cb3faca78b7
Jan Heinrich Reimer <jan.reimer@student.uni-halle.de>
2023-01-31 14:18:53 +0100

    Add aggregated stats CLI

--- web_archive_query_log/cli/stats.py
+++ web_archive_query_log/cli/stats.py
@@ -1,11 +1,14 @@
 from asyncio import run
+from math import inf
 from pathlib import Path
 
-from click import option, argument, BOOL
+from click import option, argument, BOOL, IntRange
+from pandas import DataFrame
 
 from web_archive_query_log import DATA_DIRECTORY_PATH, CDX_API_URL, LOGGER
 from web_archive_query_log.cli import main
 from web_archive_query_log.cli.util import PathParam, ServiceChoice
+from web_archive_query_log.config import SERVICES
 
 # See:
 # https://github.com/internetarchive/wayback/blob/master/wayback-cdx-server/README.md#pagination-api
@@ -51,6 +54,37 @@ def _service_name_argument():
     )
 
 
+def _all_archived_urls(
+        data_directory: Path,
+        focused: bool,
+        service: str,
+) -> int:
+    from web_archive_query_log.config import SERVICES
+    from web_archive_query_log.urls.fetch import ArchivedUrlsFetcher, \
+        UrlMatchScope
+    service_config = SERVICES[service]
+    match_scope = UrlMatchScope.PREFIX if focused else UrlMatchScope.DOMAIN
+    fetcher = ArchivedUrlsFetcher(
+        match_scope=match_scope,
+        include_status_codes={200},
+        exclude_status_codes=set(),
+        include_mime_types={"text/html"},
+        exclude_mime_types=set(),
+        cdx_api_url=CDX_API_URL,
+    )
+    if focused:
+        if len(service_config.focused_url_prefixes) == 0:
+            LOGGER.warning(
+                f"No focused URL prefixes configured for service {service}."
+            )
+    num_pages = run(fetcher.num_service_pages(
+        data_directory=data_directory,
+        focused=focused,
+        service=service_config,
+    ))
+    return num_pages * _BLOCKS_PER_PAGE * _URLS_PER_BLOCK
+
+
 @stats_group.command(
     "all-archived-urls",
     help="Get upper bound for the number of all archived URLs from "
@@ -64,30 +98,26 @@ def all_archived_urls_command(
         focused: bool,
         service: str,
 ) -> None:
-    from web_archive_query_log.config import SERVICES
-    from web_archive_query_log.urls.fetch import ArchivedUrlsFetcher, \
-        UrlMatchScope
-    service_config = SERVICES[service]
-    match_scope = UrlMatchScope.PREFIX if focused else UrlMatchScope.DOMAIN
-    fetcher = ArchivedUrlsFetcher(
-        match_scope=match_scope,
-        include_status_codes={200},
-        exclude_status_codes=set(),
-        include_mime_types={"text/html"},
-        exclude_mime_types=set(),
-        cdx_api_url=CDX_API_URL
-    )
-    if focused:
-        if len(service_config.focused_url_prefixes) == 0:
-            LOGGER.warning(
-                f"No focused URL prefixes configured for service {service}."
-            )
-    num_pages = run(fetcher.num_service_pages(
+    print(_all_archived_urls(
         data_directory=data_directory,
         focused=focused,
-        service=service_config,
+        service=service,
     ))
-    print(num_pages * _BLOCKS_PER_PAGE * _URLS_PER_BLOCK)
+
+
+def _archived_urls(
+        data_directory: Path,
+        focused: bool,
+        service: str,
+) -> int:
+    from web_archive_query_log.index import ArchivedUrlIndex
+    index = ArchivedUrlIndex(
+        data_directory=data_directory,
+        focused=focused,
+        service=service,
+    )
+    index.index()
+    return len(index)
 
 
 @stats_group.command(
@@ -103,14 +133,26 @@ def archived_urls_command(
         focused: bool,
         service: str,
 ) -> None:
-    from web_archive_query_log.index import ArchivedUrlIndex
-    index = ArchivedUrlIndex(
+    print(_archived_urls(
+        data_directory=data_directory,
+        focused=focused,
+        service=service,
+    ))
+
+
+def _archived_query_urls(
+        data_directory: Path,
+        focused: bool,
+        service: str,
+) -> int:
+    from web_archive_query_log.index import ArchivedQueryUrlIndex
+    index = ArchivedQueryUrlIndex(
         data_directory=data_directory,
         focused=focused,
         service=service,
     )
     index.index()
-    print(len(index))
+    return len(index)
 
 
 @stats_group.command(
@@ -125,14 +167,26 @@ def archived_query_urls_command(
         focused: bool,
         service: str,
 ) -> None:
-    from web_archive_query_log.index import ArchivedQueryUrlIndex
-    index = ArchivedQueryUrlIndex(
+    print(_archived_query_urls(
+        data_directory=data_directory,
+        focused=focused,
+        service=service,
+    ))
+
+
+def _archived_raw_serps(
+        data_directory: Path,
+        focused: bool,
+        service: str,
+) -> int:
+    from web_archive_query_log.index import ArchivedRawSerpIndex
+    index = ArchivedRawSerpIndex(
         data_directory=data_directory,
         focused=focused,
         service=service,
     )
     index.index()
-    print(len(index))
+    return len(index)
 
 
 @stats_group.command(
@@ -147,14 +201,26 @@ def archived_raw_serps_command(
         focused: bool,
         service: str,
 ) -> None:
-    from web_archive_query_log.index import ArchivedRawSerpIndex
-    index = ArchivedRawSerpIndex(
+    print(_archived_raw_serps(
+        data_directory=data_directory,
+        focused=focused,
+        service=service,
+    ))
+
+
+def _archived_parsed_serps(
+        data_directory: Path,
+        focused: bool,
+        service: str,
+) -> int:
+    from web_archive_query_log.index import ArchivedParsedSerpIndex
+    index = ArchivedParsedSerpIndex(
         data_directory=data_directory,
         focused=focused,
         service=service,
     )
     index.index()
-    print(len(index))
+    return len(index)
 
 
 @stats_group.command(
@@ -169,14 +235,26 @@ def archived_parsed_serps_command(
         focused: bool,
         service: str,
 ) -> None:
-    from web_archive_query_log.index import ArchivedParsedSerpIndex
-    index = ArchivedParsedSerpIndex(
+    print(_archived_parsed_serps(
+        data_directory=data_directory,
+        focused=focused,
+        service=service,
+    ))
+
+
+def _archived_raw_search_results(
+        data_directory: Path,
+        focused: bool,
+        service: str,
+) -> int:
+    from web_archive_query_log.index import ArchivedRawSearchResultIndex
+    index = ArchivedRawSearchResultIndex(
         data_directory=data_directory,
         focused=focused,
         service=service,
     )
     index.index()
-    print(len(index))
+    return len(index)
 
 
 @stats_group.command(
@@ -192,19 +270,25 @@ def archived_raw_search_results_command(
         focused: bool,
         service: str,
 ) -> None:
-    from web_archive_query_log.index import ArchivedRawSearchResultIndex
-    index = ArchivedRawSearchResultIndex(
+    print(_archived_raw_search_results(
         data_directory=data_directory,
         focused=focused,
         service=service,
-    )
-    index.index()
-    print(len(index))
+    ))
+
+
+def _archived_parsed_search_results(
+        data_directory: Path,
+        focused: bool,
+        service: str,
+) -> int:
+    raise NotImplementedError()
 
 
 @stats_group.command(
     "archived-parsed-search-results",
-    help="Parse search results from raw search result contents.",
+    help="Get number of parsed search results "
+         "from raw search result contents.",
 )
 @_data_directory_option()
 @_focused_argument()
@@ -214,4 +298,113 @@ def archived_parsed_search_results_command(
         focused: bool,
         service: str,
 ) -> None:
-    raise NotImplementedError()
+    print(_archived_parsed_search_results(
+        data_directory=data_directory,
+        focused=focused,
+        service=service,
+    ))
+
+
+@stats_group.command(
+    "all",
+    help="Get all stats for all services.",
+)
+@_data_directory_option()
+@_focused_argument()
+@option(
+    "-o", "--output", "--output-path",
+    type=PathParam(
+        exists=False,
+        file_okay=True,
+        dir_okay=False,
+        writable=True,
+        readable=False,
+        resolve_path=True,
+        path_type=Path,
+    ),
+    required=False,
+)
+@option(
+    "-k", "--top-services",
+    type=IntRange(min=1),
+    required=False,
+)
+def all_stats_command(
+        data_directory: Path,
+        focused: bool,
+        output: Path | None,
+        top_services: int | None,
+) -> None:
+    services = SERVICES.values()
+    services = sorted(services, key=lambda service: service.alexa_rank or inf)
+    if top_services is not None:
+        services = services[:top_services]
+    results: dict[str, dict[str, int]] = {}
+    for service in services:
+        service_results: dict[str, int] = {}
+        print(f"\033[1mService: {service.name}\033[0m")
+        print(f"✔ Alexa rank: {service.alexa_rank}")
+        service_results["all-archived-urls"] = _all_archived_urls(
+            data_directory,
+            focused,
+            service.name,
+        )
+        print(f"✔ Available Archived URLs: "
+              f"{service_results['all-archived-urls']}")
+        service_results["archived-urls"] = _archived_urls(
+            data_directory,
+            focused,
+            service.name,
+        )
+        print(f"✔ Archived URLs: "
+              f"{service_results['archived-urls']}")
+        service_results["archived-query-urls"] = _archived_query_urls(
+            data_directory,
+            focused,
+            service.name,
+        )
+        print(f"✔ Archived query URLs: "
+              f"{service_results['archived-query-urls']}")
+        service_results["archived-raw-serps"] = _archived_raw_serps(
+            data_directory,
+            focused,
+            service.name,
+        )
+        print(f"✔ Archived raw SERPs: "
+              f"{service_results['archived-raw-serps']}")
+        service_results["archived-parsed-serps"] = _archived_parsed_serps(
+            data_directory,
+            focused,
+            service.name,
+        )
+        print(f"✔ Archived parsed SERPs: "
+              f"{service_results['archived-parsed-serps']}")
+        service_results["archived-raw-search-results"] = \
+            _archived_raw_search_results(
+                data_directory,
+                focused,
+                service.name,
+            )
+        print(f"✔ Archived raw search results: "
+              f"{service_results['archived-raw-search-results']}")
+        # service_results["archived-parsed-search-results"] = \
+        #     _archived_parsed_search_results(
+        #         data_directory,
+        #         focused,
+        #         service.name,
+        #     )
+        # print(f"✔ Archived parsed search results: "
+        #         f"{service_results['archived-parsed-search-results']}")
+        print()
+        results[service.name] = service_results
+
+    if output is not None:
+        df = DataFrame([
+            {
+                "service": service_name,
+                **service_results,
+            }
+            for service_name, service_results in results.items()
+        ])
+        df.to_csv(output, index=False)
+        print(f"Statistics saved to {output}.")
