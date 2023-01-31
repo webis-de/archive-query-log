997f58878adfd9cb5db18a9de0a014500763e014
Jan Heinrich Reimer <jan.reimer@student.uni-halle.de>
2023-01-31 13:01:53 +0100

    Migrate stats CLI to use ID indexes

--- web_archive_query_log/cli/stats.py
+++ web_archive_query_log/cli/stats.py
@@ -1,11 +1,7 @@
 from asyncio import run
-from gzip import GzipFile
 from pathlib import Path
 
 from click import option, argument, STRING, IntRange, BOOL
-from diskcache import Cache
-from fastwarc import ArchiveIterator, WarcRecordType
-from tqdm.auto import tqdm
 
 from web_archive_query_log import DATA_DIRECTORY_PATH, CDX_API_URL, LOGGER
 from web_archive_query_log.cli import main
@@ -85,8 +81,6 @@ def all_archived_urls_command(
         data_directory: Path,
         focused: bool,
         service: str,
-        domain: str | None,
-        cdx_page: int | None,
 ) -> None:
     from web_archive_query_log.config import SERVICES
     from web_archive_query_log.urls.fetch import ArchivedUrlsFetcher, \
@@ -110,137 +104,31 @@ def all_archived_urls_command(
         data_directory=data_directory,
         focused=focused,
         service=service_config,
-        domain=domain,
-        cdx_page=cdx_page,
     ))
     print(num_pages * _BLOCKS_PER_PAGE * _URLS_PER_BLOCK)
 
 
-def _count_jsonl_path(
-        data_directory: Path,
-        path: Path,
-) -> int:
-    cache = Cache(str(DATA_DIRECTORY_PATH / "stats-cache"))
-    if path not in cache:
-        with GzipFile(path, "r") as file:
-            cache[path] = sum(1 for _ in file)
-    return cache[path]
-
-
-def _count_jsonl(
-        data_directory: Path,
-        focused: bool,
-        type: str,
-        service: str,
-        domain: str | None,
-        cdx_page: int | None,
-) -> int:
-    glob_paths = []
-    if focused:
-        glob_paths.append("focused")
-    glob_paths.append(type)
-    glob_paths.append(service)
-    if domain is not None:
-        glob_paths.append(f"{domain}*")
-    else:
-        glob_paths.append("*")
-    if cdx_page is not None:
-        glob_paths.append(f"*{cdx_page}.jsonl.gz")
-    else:
-        glob_paths.append(f"*.jsonl.gz")
-    glob_path = "/".join(glob_paths)
-    total_paths = sum(1 for _ in data_directory.glob(glob_path))
-    count = 0
-    paths = data_directory.glob(glob_path)
-    paths = tqdm(
-        paths,
-        total=total_paths,
-        desc="Count JSON lines",
-        unit="file",
-    )
-    for path in paths:
-        count += _count_jsonl_path(data_directory, path)
-    return count
-
-
-def _count_warc_path(
-        data_directory: Path,
-        path: Path,
-) -> int:
-    cache = Cache(str(DATA_DIRECTORY_PATH / "stats-cache"))
-    if path not in cache:
-        with path.open("rb") as file:
-            iterator = ArchiveIterator(
-                file,
-                record_types=WarcRecordType.response,
-            )
-            cache[path] += sum(1 for _ in iterator)
-
-    return cache[path]
-
-
-def _count_warc(
-        data_directory: Path,
-        focused: bool,
-        type: str,
-        service: str,
-        domain: str | None,
-        cdx_page: int | None,
-) -> int:
-    glob_paths = []
-    if focused:
-        glob_paths.append("focused")
-    glob_paths.append(type)
-    glob_paths.append(service)
-    if domain is not None:
-        glob_paths.append(f"{domain}*")
-    else:
-        glob_paths.append("*")
-    if cdx_page is not None:
-        glob_paths.append(f"*{cdx_page}")
-    else:
-        glob_paths.append("*")
-    glob_paths.append(f"*.warc.gz")
-    glob_path = "/".join(glob_paths)
-    total_paths = sum(1 for _ in data_directory.glob(glob_path))
-    count = 0
-    paths = data_directory.glob(glob_path)
-    paths = tqdm(
-        paths,
-        total=total_paths,
-        desc="Count WARC responses",
-        unit="file",
-    )
-    for path in paths:
-        count += _count_warc_path(data_directory, path)
-    return count
-
-
 @stats_group.command(
     "archived-urls",
-    help="Get number of fetched  archived URLs from "
+    help="Get number of fetched archived URLs from "
          "the Wayback Machine's CDX API.",
 )
 @_data_directory_option()
 @_focused_argument()
 @_service_name_argument()
-@_domain_argument()
-@_cdx_page_argument()
 def archived_urls_command(
         data_directory: Path,
         focused: bool,
         service: str,
-        domain: str | None,
-        cdx_page: int | None,
 ) -> None:
-    print(_count_jsonl(
+    from web_archive_query_log.index import ArchivedUrlIndex
+    index = ArchivedUrlIndex(
         data_directory=data_directory,
         focused=focused,
-        type="archived-urls",
         service=service,
-        domain=domain,
-        cdx_page=cdx_page,
-    ))
+    )
+    index.index()
+    print(len(index))
 
 
 @stats_group.command(
@@ -250,23 +138,19 @@ def archived_urls_command(
 @_data_directory_option()
 @_focused_argument()
 @_service_name_argument()
-@_domain_argument()
-@_cdx_page_argument()
 def archived_query_urls_command(
         data_directory: Path,
         focused: bool,
         service: str,
-        domain: str | None,
-        cdx_page: int | None,
 ) -> None:
-    print(_count_jsonl(
+    from web_archive_query_log.index import ArchivedQueryUrlIndex
+    index = ArchivedQueryUrlIndex(
         data_directory=data_directory,
         focused=focused,
-        type="archived-query-urls",
         service=service,
-        domain=domain,
-        cdx_page=cdx_page,
-    ))
+    )
+    index.index()
+    print(len(index))
 
 
 @stats_group.command(
@@ -276,23 +160,19 @@ def archived_query_urls_command(
 @_data_directory_option()
 @_focused_argument()
 @_service_name_argument()
-@_domain_argument()
-@_cdx_page_argument()
 def archived_raw_serps_command(
         data_directory: Path,
         focused: bool,
         service: str,
-        domain: str | None,
-        cdx_page: int | None,
 ) -> None:
-    print(_count_warc(
+    from web_archive_query_log.index import ArchivedRawSerpIndex
+    index = ArchivedRawSerpIndex(
         data_directory=data_directory,
         focused=focused,
-        type="archived-raw-serps",
         service=service,
-        domain=domain,
-        cdx_page=cdx_page,
-    ))
+    )
+    index.index()
+    print(len(index))
 
 
 @stats_group.command(
@@ -302,23 +182,19 @@ def archived_raw_serps_command(
 @_data_directory_option()
 @_focused_argument()
 @_service_name_argument()
-@_domain_argument()
-@_cdx_page_argument()
 def archived_parsed_serps_command(
         data_directory: Path,
         focused: bool,
         service: str,
-        domain: str | None,
-        cdx_page: int | None,
 ) -> None:
-    print(_count_jsonl(
+    from web_archive_query_log.index import ArchivedParsedSerpIndex
+    index = ArchivedParsedSerpIndex(
         data_directory=data_directory,
         focused=focused,
-        type="archived-parsed-serps",
         service=service,
-        domain=domain,
-        cdx_page=cdx_page,
-    ))
+    )
+    index.index()
+    print(len(index))
 
 
 @stats_group.command(
@@ -329,16 +205,19 @@ def archived_parsed_serps_command(
 @_data_directory_option()
 @_focused_argument()
 @_service_name_argument()
-@_domain_argument()
-@_cdx_page_argument()
 def archived_raw_search_results_command(
         data_directory: Path,
         focused: bool,
         service: str,
-        domain: str | None,
-        cdx_page: int | None,
 ) -> None:
-    raise NotImplementedError()
+    from web_archive_query_log.index import ArchivedRawSearchResultIndex
+    index = ArchivedRawSearchResultIndex(
+        data_directory=data_directory,
+        focused=focused,
+        service=service,
+    )
+    index.index()
+    print(len(index))
 
 
 @stats_group.command(
@@ -348,13 +227,9 @@ def archived_raw_search_results_command(
 @_data_directory_option()
 @_focused_argument()
 @_service_name_argument()
-@_domain_argument()
-@_cdx_page_argument()
 def archived_parsed_search_results_command(
         data_directory: Path,
         focused: bool,
         service: str,
-        domain: str | None,
-        cdx_page: int | None,
 ) -> None:
     raise NotImplementedError()
