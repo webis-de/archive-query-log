6e350a01d10046a7c8aecee04295bedbc24455db
Jan Heinrich Reimer <jan.reimer@student.uni-halle.de>
2023-02-10 15:11:27 +0100

    Add project dir reference

--- web_archive_query_log/__init__.py
+++ web_archive_query_log/__init__.py
@@ -1,7 +1,8 @@
 from logging import getLogger
 from pathlib import Path
 
-DATA_DIRECTORY_PATH = Path(__file__).parent.parent / "data"
+PROJECT_DIRECTORY_PATH = Path(__file__).parent.parent
+DATA_DIRECTORY_PATH = PROJECT_DIRECTORY_PATH / "data"
 # DATA_DIRECTORY_PATH = Path("/mnt/ceph/storage/TODO")
 
 CDX_API_URL = "https://web.archive.org/cdx/search/cdx"
