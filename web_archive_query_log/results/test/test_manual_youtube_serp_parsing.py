ab80461253fc37aba65719f9602aefa953423a1d
Jan Heinrich Reimer <jan.reimer@student.uni-halle.de>
2023-02-10 22:05:01 +0100

    Simplify manual tests

--- web_archive_query_log/results/test/test_manual_youtube_serp_parsing.py
+++ web_archive_query_log/results/test/test_manual_youtube_serp_parsing.py
@@ -1,10 +1,8 @@
-from unittest import TestCase
 from web_archive_query_log.results.test.test_utils import verify_serp_parsing
 
 
-class TestYoutubeSearch(TestCase):
-    def test_chaoz_time_search(self):
-        verify_serp_parsing(
-           'https://web.archive.org/web/20220510060811id_/https://www.youtube.com/results?search_query=%21%21%21Chaoz+time%21%21%21',
-           'youtube'
-        )
+def test_chaoz_time_search():
+    verify_serp_parsing(
+        'https://web.archive.org/web/20220510060811id_/https://www.youtube.com/results?search_query=%21%21%21Chaoz+time%21%21%21',
+        'youtube'
+    )
