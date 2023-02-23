b6f52033574e9e51c44d26643a5f9da55b2b2a48
Jan Heinrich Reimer <jan.reimer@student.uni-halle.de>
2023-02-23 15:05:33 +0100

    Add version

--- archive_query_log/__init__.py
+++ archive_query_log/__init__.py
@@ -1,6 +1,8 @@
 from logging import getLogger
 from pathlib import Path
 
+__version__ = "0.1.0"
+
 PROJECT_DIRECTORY_PATH = Path(__file__).parent.parent
 DATA_DIRECTORY_PATH = PROJECT_DIRECTORY_PATH / "data"
 # DATA_DIRECTORY_PATH = Path("/mnt/ceph/storage/TODO")
