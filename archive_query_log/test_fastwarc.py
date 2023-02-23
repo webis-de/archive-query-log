ab19261b52f8c028bf6f9fc3d35af45c565f4e72
Jan Heinrich Reimer <jan.reimer@student.uni-halle.de>
2023-02-23 19:19:58 +0100

    Add fastwarc test

---
+++ archive_query_log/test_fastwarc.py
@@ -0,0 +1,22 @@
+def test_fastwarc_installed():
+    import fastwarc
+    assert fastwarc
+
+    from fastwarc import GZipStream
+    assert GZipStream
+
+    from fastwarc import FileStream
+    assert FileStream
+
+    from fastwarc import ArchiveIterator
+    assert ArchiveIterator
+
+    from fastwarc import WarcRecordType
+    assert WarcRecordType
+
+    from fastwarc import WarcRecord
+    assert WarcRecord
+
+    from fastwarc.stream_io import PythonIOStreamAdapter
+    assert PythonIOStreamAdapter
+
