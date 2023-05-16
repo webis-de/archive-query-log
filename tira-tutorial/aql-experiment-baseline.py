ebbb9a97b167b6570acf2d6981499c2f60e25a6f
Jan Heinrich Reimer <jan.reimer@student.uni-halle.de>
2023-05-16 08:59:39 +0200

    Fix data

--- tira-tutorial/aql-experiment-baseline.py
+++ tira-tutorial/aql-experiment-baseline.py
@@ -1,24 +1,39 @@
 #!/usr/bin/env python3
-import argparse
-import pandas as pd
-from glob import glob
-import json
+
+from argparse import ArgumentParser
+from json import dump
+from pathlib import Path
+
+from pandas import concat, read_json
 
 
 def parse_args():
-    parser = argparse.ArgumentParser(description='Archive Query Log Demo')
+    parser = ArgumentParser(description="Archive Query Log Demo")
 
-    parser.add_argument('--input', type=str, help='The directory with the input data (i.e., a directory containing serps and results).', required=True)
-    parser.add_argument('--output', type=str, help='The output will be stored in this directory.', required=True)
-    
+    parser.add_argument(
+        '--input',
+        type=Path,
+        help="The directory with the input data "
+             "(i.e., a directory containing serps and results).",
+        required=True,
+    )
+    parser.add_argument(
+        "--output",
+        type=Path,
+        help="The output will be stored in this directory.",
+        required=True,
+    )
+
     return parser.parse_args()
 
 
-def main(input_dir, output_dir):
-    df = pd.concat([pd.read_json(i, lines=True) for i in glob(f'{input_dir}/serps/part*.gz')])
-    json.dump(df.service.value_counts().to_dict(), open(output_dir + '/results.json', 'w'))
+def main(input_dir: Path, output_dir: Path):
+    paths = input_dir.glob("serps/part*.gz")
+    df = concat([read_json(path, lines=True) for path in paths])
+    with (output_dir / "results.json").open("w") as file:
+        dump(df["search_provider_name"].value_counts().to_dict(), file)
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     args = parse_args()
     main(args.input, args.output)
-    
