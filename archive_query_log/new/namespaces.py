cc2fb42a0a043f2d38496fd70c15854d39e4d1d6
Jan Heinrich Reimer <jan.reimer@student.uni-halle.de>
2023-08-01 12:39:56 +0200

    Add UUID namespaces

---
+++ archive_query_log/new/namespaces.py
@@ -0,0 +1,16 @@
+from uuid import uuid5, NAMESPACE_URL
+
+NAMESPACE_AQL = uuid5(NAMESPACE_URL, "aql")
+NAMESPACE_ARCHIVE = uuid5(NAMESPACE_AQL, "archive")
+NAMESPACE_PROVIDER = uuid5(NAMESPACE_AQL, "provider")
+NAMESPACE_FILTER = uuid5(NAMESPACE_AQL, "filter")
+NAMESPACE_CAPTURE = uuid5(NAMESPACE_AQL, "capture")
+NAMESPACE_SERP = uuid5(NAMESPACE_AQL, "serp")
+NAMESPACE_RESULT = uuid5(NAMESPACE_AQL, "result")
+NAMESPACE_url_query_parser = uuid5(NAMESPACE_AQL, "url_query_parser")
+NAMESPACE_url_page_parser = uuid5(NAMESPACE_AQL, "url_page_parser")
+NAMESPACE_url_offset_parser = uuid5(NAMESPACE_AQL, "url_offset_parser")
+NAMESPACE_url_language_parser = uuid5(NAMESPACE_AQL, "url_language_parser")
+NAMESPACE_serp_query_parser = uuid5(NAMESPACE_AQL, "serp_query_parser")
+NAMESPACE_serp_snippets_parser = uuid5(NAMESPACE_AQL, "serp_snippets_parser")
+NAMESPACE_serp_direct_answer_parser = uuid5(NAMESPACE_AQL, "serp_direct_answer_parser")
