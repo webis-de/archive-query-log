607e2fd3502d93467fde86bb6f92c43c746cdd3b
Jan Heinrich Reimer <heinrich.reimer@uni-jena.de>
2023-08-08 10:14:09 +0200

    Draft search provider import

--- archive_query_log/new/cli/provider.py
+++ archive_query_log/new/cli/provider.py
@@ -1,8 +1,17 @@
 from datetime import datetime
+from pathlib import Path
+from typing import Sequence, MutableMapping
 from uuid import uuid5
 
-from click import group, argument, option, echo, Choice
+from click import group, argument, option, echo, Choice, Path as PathType, \
+    prompt
+from diskcache import Index
+from elasticsearch.helpers import parallel_bulk
+from tqdm.auto import tqdm
+from yaml import safe_load
+from whois import whois
 
+from archive_query_log import DATA_DIRECTORY_PATH
 from archive_query_log.new.cli.validation import validate_domains
 from archive_query_log.new.config import CONFIG
 from archive_query_log.new.namespaces import NAMESPACE_PROVIDER
@@ -129,4 +138,126 @@ def add(
     )
     echo(f"Refresh index {CONFIG.es_index_providers.name}.")
     CONFIG.es.indices.refresh(index=CONFIG.es_index_providers.name)
+    echo("Done.")
+
+
+def _provider_name(main_domain: str,
+                   provider_names: MutableMapping[str, str],
+                   review: bool) -> str:
+    if main_domain in provider_names:
+        if not review:
+            return provider_names[main_domain]
+        else:
+            provider_name_suggest = provider_names[main_domain]
+    else:
+        main_domain_info = whois(main_domain)
+        main_org: str | None = main_domain_info.org
+        if main_org is not None:
+            for restricted_phrase in ["redacted", "privacy",
+                                      "domain protection", "not disclosed",
+                                      "identity protection",
+                                      "domains by proxy"]:
+                if restricted_phrase in main_org.casefold():
+                    main_org = None
+                    break
+        if main_org is not None:
+            for suffix in ["Inc", "LLC", "Ltd", "LTD", "GmbH", "AG", "S.A",
+                           "SE", "Co", "Pty", "B.V", "S.L", "S.R.L", "S.A.S",
+                           "SAS", "AB", "&"]:
+                main_org = main_org.removesuffix(f", {suffix}.")
+                main_org = main_org.removesuffix(f", {suffix}")
+                main_org = main_org.removesuffix(f" {suffix}.")
+                main_org = main_org.removesuffix(f" {suffix}")
+        provider_name_suggest = main_org
+    provider_name = prompt(
+        f"Please enter the search provider name for https://{main_domain}",
+        type=str, default=provider_name_suggest)
+    if provider_name.strip() == "-":
+        return
+    provider_names[main_domain] = provider_name
+    return provider_name
+
+
+@provider.command()
+@option("-s", "--services-file", "services_path",
+        type=PathType(path_type=Path, exists=True, file_okay=True,
+                      dir_okay=False, readable=True, resolve_path=True,
+                      allow_dash=False),
+        default=DATA_DIRECTORY_PATH / "selected-services.yaml")
+@option("-c", "--cache-dir", "cache_path",
+        type=PathType(path_type=Path, exists=False, file_okay=False,
+                      dir_okay=True, readable=True, writable=True,
+                      resolve_path=True, allow_dash=False),
+        default=DATA_DIRECTORY_PATH / "provider-names")
+@option("--review/--no-review", "review", type=bool,
+        default=False)
+def import_providers(services_path: Path, cache_path: Path,
+                     review: bool) -> None:
+    echo("Load providers from services file.")
+    with services_path.open("r") as file:
+        services: Sequence[dict] = safe_load(file)
+    echo(f"Found {len(services)} service definitions.")
+
+    # noinspection PyTypeChecker
+    # services = tqdm(services, desc="Convert services")
+
+    provider_names: MutableMapping[str, str] = Index(str(cache_path))
+
+    documents = []
+    for i, service in enumerate(services):
+        main_domain = service["alexa_domain"]
+        print(i)
+        provider_name = _provider_name(main_domain, provider_names, review)
+
+        # document = {
+        #     "id": provider_id,
+        #     "name": name,
+        #     "description": description,
+        #     "website_type": website_type,
+        #     "content_type": content_type,
+        #     "interface_annotations": {
+        #         "has_input_field": has_input_field,
+        #         "has_search_form": has_search_form,
+        #         "has_search_div": has_search_div,
+        #     },
+        #     "domains": list(domains),
+        #     "url_path_prefixes": list(url_path_prefixes),
+        #     "last_modified": datetime.now().strftime("%Y-%m-%dT%H:%M:%SZ"),
+        # }
+        # documents.append(document)
+
+    raise NotImplementedError()
+
+    create_index(CONFIG.es_index_providers)
+    operations = (
+        {
+            "_op_type": "create",
+            "_index": CONFIG.es_index_providers.name,
+            "_id": document["id"],
+            **document,
+        }
+        for document in documents
+    )
+    has_errors = False
+    # noinspection PyTypeChecker
+    for success, info in tqdm(
+            parallel_bulk(
+                CONFIG.es,
+                operations,
+                ignore_status=[409],
+            ),
+            desc="Adding providers",
+            total=len(documents),
+            unit="capture",
+    ):
+        if not success:
+            if info["create"]["status"] != 409:
+                echo("Indexing error:", info, err=True)
+                has_errors = True
+    if has_errors:
+        raise RuntimeError("Indexing errors occurred.")
+
+    echo(f"Refresh index {CONFIG.es_index_providers.name}.")
+    CONFIG.es.indices.refresh(index=CONFIG.es_index_providers.name)
+
     echo("Done.")
