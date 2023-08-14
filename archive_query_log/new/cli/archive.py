a8be35d8518095bd3829d8a2fc42e7eda3852eb4
Jan Heinrich Reimer <heinrich.reimer@uni-jena.de>
2023-08-14 16:32:12 +0200

    Improve archives CLI

--- archive_query_log/new/cli/archive.py
+++ archive_query_log/new/cli/archive.py
@@ -1,20 +1,79 @@
-from datetime import datetime
 from urllib.parse import urljoin
-from uuid import uuid5
+from uuid import uuid4
 
-from click import group, argument, option, echo, IntRange
-from elasticsearch.helpers import parallel_bulk
-from tqdm.auto import trange, tqdm
+from click import group, argument, option, echo, IntRange, prompt
+from elasticsearch_dsl import Search
+from elasticsearch_dsl.query import Term
+from elasticsearch_dsl.response import Response
+from tqdm.auto import tqdm
 
-from archive_query_log.new.config import CONFIG
 from archive_query_log.new.http import session
-from archive_query_log.new.namespaces import NAMESPACE_ARCHIVE
-from archive_query_log.new.utils.es import create_index
+from archive_query_log.new.orm import Archive
 
 
 @group()
 def archive():
     pass
+
+
+def _add_archive(
+        name: str | None,
+        description: str | None,
+        cdx_api_url: str,
+        memento_api_url: str,
+        no_merge: bool = False,
+        auto_merge: bool = False,
+) -> None:
+    last_built_sources = None
+    existing_archive_search: Search = (
+        Archive.search()
+        .query(
+            Term(cdx_api_url=cdx_api_url) |
+            Term(memento_api_url=memento_api_url)
+        )
+    )
+    existing_archive_response: Response = existing_archive_search.execute()
+    if existing_archive_response.hits.total.value > 0:
+        if no_merge:
+            return
+        existing_archive: Archive = existing_archive_response[0]
+        archive_id = existing_archive.meta.id
+        if auto_merge:
+            should_merge = True
+        else:
+            echo(f"Archive {archive_id} already exists with "
+                 f"conflicting API endpoints.")
+            add_to_existing = prompt("Merge with existing archive? [y/N]",
+                                     type=str, default="n", show_default=False)
+            should_merge = add_to_existing.lower() == "y"
+        if not should_merge:
+            return
+
+        if name is None:
+            name = existing_archive.name
+        if description is None:
+            description = existing_archive.description
+
+        if cdx_api_url == existing_archive.cdx_api_url or \
+                memento_api_url == existing_archive.memento_api_url:
+            last_built_sources = existing_archive.last_built_sources
+
+        if not auto_merge:
+            echo(f"Update archive {archive_id}.")
+    else:
+        archive_id = str(uuid4())
+        if not no_merge and not auto_merge:
+            echo(f"Add new archive {archive_id}.")
+
+    archive = Archive(
+        meta={"id": archive_id},
+        name=name,
+        description=description,
+        cdx_api_url=cdx_api_url,
+        memento_api_url=memento_api_url,
+        last_built_sources=last_built_sources,
+    )
+    archive.save()
 
 
 @archive.command()
@@ -30,25 +89,20 @@ def add(
         cdx_api_url: str,
         memento_api_url: str,
 ) -> None:
-    create_index(CONFIG.es_index_archives)
-    archive_id = str(uuid5(NAMESPACE_ARCHIVE, name))
-    document = {
-        "id": archive_id,
-        "name": name,
-        "description": description,
-        "cdx_api_url": cdx_api_url,
-        "memento_api_url": memento_api_url,
-        "last_modified": datetime.now().strftime("%Y-%m-%dT%H:%M:%SZ"),
-    }
-    echo(f"Add archive {archive_id}.")
-    CONFIG.es.index(
-        index=CONFIG.es_index_archives.name,
-        id=archive_id,
-        document=document,
+    Archive.init()
+    Archive().index.refresh()
+    _add_archive(
+        name=name,
+        description=description,
+        cdx_api_url=cdx_api_url,
+        memento_api_url=memento_api_url,
     )
-    echo(f"Refresh index {CONFIG.es_index_archives.name}.")
-    CONFIG.es.indices.refresh(index=CONFIG.es_index_archives.name)
-    echo("Done.")
+    Archive().index.refresh()
+
+
+@archive.group("import")
+def import_():
+    pass
 
 
 ARCHIVE_IT_METADATA_FIELDS = [
@@ -66,15 +120,20 @@ ARCHIVE_IT_METADATA_FIELDS = [
 ]
 
 
-@archive.command()
+@import_.command()
 @option("--api-url", type=str, required=True,
         default="https://partner.archive-it.org", metavar="URL")
 @option("--wayback-url", type=str, required=True,
         default="https://wayback.archive-it.org", metavar="URL")
 @option("--page-size", type=IntRange(min=1), required=True,
         default=100)
-def add_archive_it_collections(api_url: str, wayback_url: str,
-                               page_size: int) -> None:
+@option("--no-merge", is_flag=True, default=False, type=bool)
+@option("--auto-merge", is_flag=True, default=False, type=bool)
+def archive_it(api_url: str, wayback_url: str, page_size: int, no_merge: bool,
+               auto_merge: bool) -> None:
+    Archive.init()
+    Archive().index.refresh()
+
     echo("Load Archive-It collections.")
     collections_api_url = urljoin(api_url, "/api/collection")
     response = session.get(collections_api_url,
@@ -82,10 +141,10 @@ def add_archive_it_collections(api_url: str, wayback_url: str,
     num_collections = int(response.headers["Total-Row-Count"])
     echo(f"Found {num_collections} collections on Archive-It.")
 
-    documents = []
-    offset_range = trange(0, num_collections, page_size,
-                          desc="Load collections")
     # noinspection PyTypeChecker
+    progress = tqdm(total=num_collections, desc="Import archives",
+                    unit="archives", disable=not auto_merge and not no_merge)
+    offset_range = range(0, num_collections, page_size)
     for offset in offset_range:
         response = session.get(collections_api_url,
                                params={"limit": page_size, "offset": offset,
@@ -104,52 +163,15 @@ def add_archive_it_collections(api_url: str, wayback_url: str,
                             f"{metadata_field}: {title['value']}")
             description_parts.append(f"Archive-It ID: {archive_it_id}")
             description = "\n".join(description_parts)
-
-            archive_id = str(uuid5(NAMESPACE_ARCHIVE, name))
             cdx_api_url = urljoin(wayback_url, f"{archive_it_id}/timemap/cdx")
             memento_api_url = urljoin(wayback_url, f"{archive_it_id}")
-
-            document = {
-                "id": archive_id,
-                "name": name,
-                "description": description,
-                "cdx_api_url": cdx_api_url,
-                "memento_api_url": memento_api_url,
-                "last_modified": datetime.now().strftime("%Y-%m-%dT%H:%M:%SZ"),
-            }
-            documents.append(document)
-    echo(f"Found {len(documents)} archives (Archive-It collections).")
-
-    create_index(CONFIG.es_index_archives)
-    operations = (
-        {
-            "_op_type": "create",
-            "_index": CONFIG.es_index_archives.name,
-            "_id": document["id"],
-            **document,
-        }
-        for document in documents
-    )
-    has_errors = False
-    # noinspection PyTypeChecker
-    for success, info in tqdm(
-            parallel_bulk(
-                CONFIG.es,
-                operations,
-                ignore_status=[409],
-            ),
-            desc="Adding archives",
-            total=len(documents),
-            unit="capture",
-    ):
-        if not success:
-            if info["create"]["status"] != 409:
-                echo("Indexing error:", info, err=True)
-                has_errors = True
-    if has_errors:
-        raise RuntimeError("Indexing errors occurred.")
-
-    echo(f"Refresh index {CONFIG.es_index_archives.name}.")
-    CONFIG.es.indices.refresh(index=CONFIG.es_index_archives.name)
-
-    echo("Done.")
+            _add_archive(
+                name=name,
+                description=description,
+                cdx_api_url=cdx_api_url,
+                memento_api_url=memento_api_url,
+                no_merge=no_merge,
+                auto_merge=auto_merge,
+            )
+            progress.update(1)
+    Archive().index.refresh()
