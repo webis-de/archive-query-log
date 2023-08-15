1ecf5d6771e92d419a92120338cd0750250e81ba
Jan Heinrich Reimer <heinrich.reimer@uni-jena.de>
2023-08-15 09:43:52 +0200

    Load version from package data

--- archive_query_log/__init__.py
+++ archive_query_log/__init__.py
@@ -1,11 +1,12 @@
 from logging import getLogger
 from pathlib import Path
 
-__version__ = "0.1.0"
+from importlib_metadata import version
+
+__version__ = version("archive_query_log")
 
 PROJECT_DIRECTORY_PATH = Path(__file__).parent.parent
 DATA_DIRECTORY_PATH = PROJECT_DIRECTORY_PATH / "data"
-# DATA_DIRECTORY_PATH = Path("/mnt/ceph/storage/TODO")
 
 CDX_API_URL = "https://web.archive.org/cdx/search/cdx"
 
