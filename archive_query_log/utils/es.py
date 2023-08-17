36d35837edb651341d153e2a7509f9f65c0a221c
Jan Heinrich Reimer <heinrich.reimer@uni-jena.de>
2023-08-17 11:51:37 +0200

    Add ES utils

---
+++ archive_query_log/utils/es.py
@@ -0,0 +1,26 @@
+from typing import Iterator, TypeVar
+from warnings import warn
+
+from elasticsearch import NotFoundError
+from elasticsearch_dsl import Document
+
+DocumentType = TypeVar("DocumentType", bound=Document)
+
+
+def safe_iter_scan(it: Iterator[DocumentType]) -> Iterator[DocumentType]:
+    try:
+        for doc in it:
+            yield doc
+    except NotFoundError as e:
+        if (e.info is not None and "error" in e.info and
+                isinstance(e.info["error"], dict) and
+                "root_cause" in e.info["error"] and
+                isinstance(e.info["error"]["root_cause"], list) and
+                len(e.info["error"]["root_cause"]) > 0 and
+                isinstance(e.info["error"]["root_cause"][0], dict) and
+                "resource.id" in e.info["error"]["root_cause"][0] and
+                e.info["error"]["root_cause"][0]["resource.id"] ==
+                "search_phase_execution_exception"):
+            warn(RuntimeWarning(f"Scan expired. Stopping iteration."))
+        else:
+            raise e
