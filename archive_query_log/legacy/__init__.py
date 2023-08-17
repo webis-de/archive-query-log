0f35a01373650208c80974c99948b4014cf45b2a
Jan Heinrich Reimer <heinrich.reimer@uni-jena.de>
2023-08-17 12:34:43 +0200

    Fix circular import

--- archive_query_log/legacy/__init__.py
+++ archive_query_log/legacy/__init__.py
@@ -1,8 +1,6 @@
 from logging import getLogger
 from pathlib import Path
 
-from archive_query_log.legacy.cli import main
-
 PROJECT_DIRECTORY_PATH = Path(__file__).parent.parent
 DATA_DIRECTORY_PATH = PROJECT_DIRECTORY_PATH / "data"
 
@@ -11,4 +9,6 @@ CDX_API_URL = "https://web.archive.org/cdx/search/cdx"
 LOGGER = getLogger(__name__)
 
 if __name__ == "__main__":
+    from archive_query_log.legacy.cli import main
+
     main()
