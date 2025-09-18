17ba5fb3d4f1af6884d3642ba500fcdd96fe0dbb
Simon André Ruth <simon.andre.ruth@uni-weimar.de>
2025-09-18 11:16:50 +0200

    more kg fields

--- archive_query_log/export/knowledge_graph.py
+++ archive_query_log/export/knowledge_graph.py
@@ -20,17 +20,83 @@ from typing import Iterator
 #             "number_of_replicas": 2,
 #         }
 
+# class Archive(BaseDocument):
+#     name: str = Text()
+#     description: str = Text()
+#     cdx_api_url: str = Keyword()
+#     memento_api_url: str = Keyword()
+#     priority: float | None = RankFeature(positive_score_impact=True)
+#     should_build_sources: bool = Boolean()
+#     last_built_sources: datetime = Date(
+#         default_timezone="UTC",
+#         format="strict_date_time_no_millis",
+#     )
+
+#     class Index:
+#         settings = {
+#             "number_of_shards": 1,
+#             "number_of_replicas": 2,
+#         }
+
+# class Capture(BaseDocument):
+#     archive: InnerArchive = Object(InnerArchive)
+#     provider: InnerProvider = Object(InnerProvider)
+#     url: str = Keyword()
+#     url_key: str = Keyword()
+#     timestamp: datetime = Date(
+#         default_timezone="UTC",
+#         format="strict_date_time_no_millis",
+#     )
+#     status_code: int = Integer()
+#     digest: str = Keyword()
+#     mimetype: str | None = Keyword()
+#     filename: str | None = Keyword()
+#     offset: int | None = Integer()
+#     length: int | None = Integer()
+#     access: str | None = Keyword()
+#     redirect_url: str | None = Keyword()
+#     flags: list[str] | None = Keyword()
+#     collection: str | None = Keyword()
+#     source: str | None = Keyword()
+#     source_collection: str | None = Keyword()
+#     url_query_parser: InnerParser | None = Object(InnerParser)
+
+#     class Index:
+#         settings = {
+#             "number_of_shards": 40,
+#             "number_of_replicas": 2,
+#         }
+
 
 
 def iter_turtle_triples(provider: Provider) -> Iterator[tuple[str, str, str]]:
     entity = f"https://aql.webis.de/provider/{provider.id}"
     
+    yield(entity, "schema:identifier", provider.id)
+    yield(entity, "schema:name", provider.name)
 
     for domain in provider.domains:
         yield (entity, "aql:domain", domain)
     for url in provider.url_path_prefixes:
         yield(entity, "schema:urlPathPrefix", url)
+    #Missing aqlWikidataUrl -> to be exported with another method
 
-    yield(entity, "aql:identifier", provider.id) # TODO is this needed?
-    yield(entity, "schema:name", provider.name)
 
+def iter_turtle_triples(archive: Archive) -> Iterator[tuple[str, str, str]]:
+    entity = f"https://aql.webis.de/archive/{archive.id}"
+    
+    yield(entity, "schema:identifier", archive.id)
+    yield(entity, "schema:name", archive.name)
+    yield(entity, "aql:mementoAPIBaseURL", archive.memento_api_url)
+    yield(entity, "aql:cdxAPIBaseURL", archive.cdx_api_url)
+    #Missing aqlWikidataUrl -> to be exported with another method
+    
+def iter_turtle_triples(capture: Capture) -> Iterator[tuple[str, str, str]]:
+    entitiy = f"https://aql.webis.de/capture/{capture.id}"
+    
+    yield(entitiy, "schema:dateCreated", capture.timestamp.isoformat())
+    yield(entitiy, "schema:url", capture.url)
+    yield(entitiy, "http:statusCodeNumber", str(capture.status_code))
+    yield(entitiy, "aql:digest", capture.digest)
+    yield(entitiy, "schema:encodingFormat", capture.mimetype)
+    yield(entitiy, "aql:mementoAPIViewerURL", ) #TODO mementoapi viewer url contained in archive, where is mementoapi raw url from? -> this links to the archive where capture is archived
