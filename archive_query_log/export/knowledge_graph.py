c389a175b55ef4981c314b7f53e2770701813748
Simon André Ruth <simon.andre.ruth@uni-weimar.de>
2025-09-22 16:14:27 +0200

    continuing on serp

--- archive_query_log/export/knowledge_graph.py
+++ archive_query_log/export/knowledge_graph.py
@@ -1,97 +1,30 @@
 from typing import Iterator
-from orm import Provider, Archive, Capture
+from orm import Provider, Archive, Capture, SERP
 
-# class Provider(BaseDocument):
-#     name: str = Text()
-#     description: str = Text()
-#     exclusion_reason: str = Text()
-#     notes: str = Text()
-#     domains: list[str] = Keyword()
-#     url_path_prefixes: list[str] = Keyword()
-#     priority: float | None = RankFeature(positive_score_impact=True)
-#     should_build_sources: bool = Boolean()
-#     last_built_sources: datetime = Date(
-#         default_timezone="UTC",
-#         format="strict_date_time_no_millis",
-#     )
 
-#     class Index:
-#         settings = {
-#             "number_of_shards": 1,
-#             "number_of_replicas": 2,
-#         }
-
-# class Archive(BaseDocument):
-#     name: str = Text()
-#     description: str = Text()
-#     cdx_api_url: str = Keyword()
-#     memento_api_url: str = Keyword()
-#     priority: float | None = RankFeature(positive_score_impact=True)
-#     should_build_sources: bool = Boolean()
-#     last_built_sources: datetime = Date(
-#         default_timezone="UTC",
-#         format="strict_date_time_no_millis",
-#     )
-
-#     class Index:
-#         settings = {
-#             "number_of_shards": 1,
-#             "number_of_replicas": 2,
-#         }
-
-# class Capture(BaseDocument):
-#     archive: InnerArchive = Object(InnerArchive)
-#     provider: InnerProvider = Object(InnerProvider)
-#     url: str = Keyword()
-#     url_key: str = Keyword()
-#     timestamp: datetime = Date(
-#         default_timezone="UTC",
-#         format="strict_date_time_no_millis",
-#     )
-#     status_code: int = Integer()
-#     digest: str = Keyword()
-#     mimetype: str | None = Keyword()
-#     filename: str | None = Keyword()
-#     offset: int | None = Integer()
-#     length: int | None = Integer()
-#     access: str | None = Keyword()
-#     redirect_url: str | None = Keyword()
-#     flags: list[str] | None = Keyword()
-#     collection: str | None = Keyword()
-#     source: str | None = Keyword()
-#     source_collection: str | None = Keyword()
-#     url_query_parser: InnerParser | None = Object(InnerParser)
-
-#     class Index:
-#         settings = {
-#             "number_of_shards": 40,
-#             "number_of_replicas": 2,
-#         }
-
-# class Serp(BaseDocument):
-#     archive: InnerArchive = Object(InnerArchive)
-#     provider: InnerProvider = Object(InnerProvider)
-#     capture: InnerCapture = Object(InnerCapture)
-#     url_query: str = Text()
-#     url_query_parser: InnerParser | None = Object(InnerParser)
-#     url_page: int | None = Integer()
-#     url_page_parser: InnerParser | None = Object(InnerParser)
-#     url_offset: int | None = Integer()
-#     url_offset_parser: InnerParser | None = Object(InnerParser)
-#     # url_language: str | None = Keyword()
-#     # url_language_parser: InnerParser | None = Object(InnerParser)
-#     warc_location: WarcLocation | None = Object(WarcLocation)
-#     warc_downloader: InnerDownloader | None = Object(InnerDownloader)
-#     warc_query: str | None = Text()
-#     warc_query_parser: InnerParser | None = Object(InnerParser)
-#     warc_snippets: list[SnippetId] | None = Nested(SnippetId)
-#     warc_snippets_parser: InnerParser | None = Object(InnerParser)
-#     warc_direct_answers: list[DirectAnswerId] | None = Nested(DirectAnswerId)
-#     warc_direct_answers_parser: InnerParser | None = Object(InnerParser)
-
-#     # rendered_warc_location: WarcLocation | None = Object(WarcLocation)
-#     # rendered_warc_downloader: InnerDownloader | None = (
-#     #     Object(InnerDownloader))
+# class Serp(UuidBaseDocument):
+#     last_modified: DefaultDate
+#     archive: InnerArchive
+#     provider: InnerProvider
+#     capture: InnerCapture
+#     url_query: Text
+#     url_query_parser: InnerParser | None = None
+#     url_page: Integer | None = None
+#     url_page_parser: InnerParser | None = None
+#     url_offset: Integer | None = None
+#     url_offset_parser: InnerParser | None = None
+#     # url_language: Keyword | None = None
+#     # url_language_parser: InnerParser | None = None
+#     warc_location: WarcLocation | None = None
+#     warc_downloader: InnerDownloader | None = None
+#     warc_query: Text | None = None
+#     warc_query_parser: InnerParser | None = None
+#     warc_web_search_result_blocks: Sequence[WebSearchResultBlockId] | None = None
+#     warc_web_search_result_blocks_parser: InnerParser | None = None
+#     warc_special_contents_result_blocks: (
+#         Sequence[SpecialContentsResultBlockId] | None
+#     ) = None
+#     warc_special_contents_result_blocks_parser: InnerParser | None = None
 
 #     class Index:
 #         settings = {
@@ -100,6 +33,33 @@ from orm import Provider, Archive, Capture
 #         }
 
 
+
+# class WebSearchResultBlock(UuidBaseDocument):
+#     last_modified: DefaultDate
+#     archive: InnerArchive
+#     provider: InnerProvider
+#     serp_capture: InnerCapture
+#     serp: InnerSerp
+#     content: Text
+#     rank: Integer
+#     url: HttpUrl | None = None
+#     title: Text | None = None
+#     text: Text | None = None
+#     parser: InnerParser | None = None
+#     should_fetch_captures: bool = True
+#     last_fetched_captures: Date | None = None
+#     capture_before_serp: InnerCapture | None = None
+#     warc_location_before_serp: WarcLocation | None = None
+#     warc_downloader_before_serp: InnerDownloader | None = None
+#     capture_after_serp: InnerCapture | None = None
+#     warc_location_after_serp: WarcLocation | None = None
+#     warc_downloader_after_serp: InnerDownloader | None = None
+
+#     class Index:
+#         settings = {
+#             "number_of_shards": 20,
+#             "number_of_replicas": 2,
+#         }
 
 
 
@@ -148,4 +108,13 @@ def iter_turtle_triples(serp: SERP) -> Iterator[tuple[str, str, str]]:
     yield(entity, "schema:identifier", serp.id)
     # yield(entity, "aql:trecTaskURLQuery", ) TODO what is the trec task? is this just hypothetical?
     yield(entity, "schema:publisher", f"https://aql.webis.de/provider/{serp.provider.id}")
-    yield(entity, "schema:hasPart", ) #TODO result block. -> new naming in new branch?
+    
+    for websearchresultblock in serp.warc_web_search_result_blocks:
+        yield(entity, "schema:hasPart", f"https://aql.webis.de/websearchresultblock/{websearchresultblock.id}") # iterate over all websearchresultblocks of a serp
+        
+    for specialcontentsresultblock in serp.warc_special_contents_result_blocks:
+        yield(entity, "schema:hasPart", f"https://aql.webis.de/specialcontentsresultblock/{specialcontentsresultblock.id}") # iterate over all specialcontentsresultblocks of a serp
+        
+    #TODO how to model superclass of resultblock? we dont have a python class for it
+        
+        # Model result block as superclass in python first, then in knowledgegraph
