47203c15aaab00b0e133d4834640062449481d85
Jan Heinrich Merker <heinrich.merker@uni-jena.de>
2025-09-22 11:43:16 +0200

    Fix validator

--- archive_query_log/cli/util.py
+++ archive_query_log/cli/util.py
@@ -1,17 +1,17 @@
-from typing import Annotated
+from typing import Annotated, Any
 
 from cyclopts import Parameter
 from validators import domain as validate_domain
 
 
-def _validate_domain(domain: str) -> str:
-    valid = validate_domain(domain)
-    if not valid:
-        if isinstance(valid, bool):
-            raise ValueError(f"Not a valid domain: {domain}")
-        else:
-            raise valid
-    return domain
+def _domain_validator(type_, value: Any) -> None:
+    if value is not None:
+        valid = validate_domain(value)
+        if not valid:
+            if isinstance(valid, bool):
+                raise ValueError(f"Not a valid domain: {value}")
+            else:
+                raise valid
 
 
-Domain = Annotated[str, Parameter(validator=_validate_domain)]
+Domain = Annotated[str, Parameter(validator=_domain_validator)]
