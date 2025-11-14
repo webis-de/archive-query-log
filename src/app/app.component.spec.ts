6bdc89533c19ba34bb33e314aff3069376fd18c5
Salim Alkhaddoor <salim.alkhaddoor@uni-jena.de>
2025-11-14 13:06:39 +0100

    ci: update Node version to 24.11.0 and add npm version check in CI pipeline; update test expectation for AppComponent title

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -24,6 +24,6 @@ describe('AppComponent', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, aql-frontend');
+    expect(compiled.querySelector('h1')?.textContent).toContain('Willkommen bei AQL Frontend!');
   });
 });
