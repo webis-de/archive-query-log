6f4107fdc54395438c3eb9c7f26d48e9529d9961
Nico Mäder <maeder.n2004@gmail.com>
2025-11-16 21:31:00 +0100

    updated dependencies and deprecated code

--- app/core/settings.py
+++ app/core/settings.py
@@ -1,13 +1,12 @@
-from pydantic_settings import BaseSettings
+from pydantic_settings import BaseSettings, SettingsConfigDict
 
 
 class Settings(BaseSettings):
+    model_config = SettingsConfigDict(env_file=".env")
+
     es_host: str = "https://elasticsearch.example.com:9200"
     es_api_key: str | None = None
     es_verify: bool = False
-
-    class Config:
-        env_file = ".env"
 
 
 settings = Settings()
