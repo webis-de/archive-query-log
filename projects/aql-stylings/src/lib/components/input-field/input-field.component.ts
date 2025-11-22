f3e1b0c0d237423edbcaaed6f56e03086f9673fc
lhinueber <linda.hinueber@uni-jena.de>
2025-11-22 18:53:19 +0100

    feat: refactor input field component with Tailwind/DaisyUI CSS styling and enhanced structure add Input type and icons Refs. 8

--- projects/aql-stylings/src/lib/components/input-field/input-field.component.ts
+++ projects/aql-stylings/src/lib/components/input-field/input-field.component.ts
@@ -1,12 +1,13 @@
 import { Component, Input } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-import { AqlButtonComponent } from '../button/aql-button.component';
+
+export type InputShape = 'square' | 'circle';
 
 @Component({
   selector: 'input-field',
   standalone: true,
-  imports: [CommonModule, FormsModule, AqlButtonComponent],
+  imports: [CommonModule, FormsModule], 
   templateUrl: './input-field.component.html',
   styleUrl: './input-field.component.css',
 })
@@ -14,21 +15,18 @@ export class InputFieldComponent {
   @Input() label = ''; 
   @Input() placeholder = 'input';
   @Input() value = '';
-  
- 
   @Input() disabled = false; 
-
-
-  @Input() showIcon = true;
-  @Input() showButton = true;
   
+  // Default-Icon.
+  @Input() showIcon = true; 
+  
+  // (text, password, date, email, etc.)
+  @Input() type = 'text';
 
-  @Input() buttonText = 'Search';
+  @Input() shape: InputShape = 'square'; 
 
   onInput(event: Event) {
     const target = event.target as HTMLInputElement;
     this.value = target.value;
   }
-  
-
 }
