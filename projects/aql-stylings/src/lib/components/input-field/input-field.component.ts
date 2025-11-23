8a9df490d710c378b175056fb0b3c03be37afe53
lhinueber <linda.hinueber@uni-jena.de>
2025-11-23 17:33:38 +0100

    refactor: enhance input field component with DaisyUI size, color options, spacings and usage of icon Ref. #8

--- projects/aql-stylings/src/lib/components/input-field/input-field.component.ts
+++ projects/aql-stylings/src/lib/components/input-field/input-field.component.ts
@@ -2,6 +2,9 @@ import { Component, Input } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 
+// Definition der verfügbaren DaisyUI Größen und Farben
+export type InputSize = 'xs' | 'sm' | 'md' | 'lg';
+export type InputColor = 'primary' | 'secondary' | 'accent' | 'info' | 'success' | 'warning' | 'error' | 'ghost' | 'bordered' | 'neutral';
 export type InputShape = 'square' | 'circle';
 
 @Component({
@@ -19,11 +22,17 @@ export class InputFieldComponent {
   
   // Default-Icon.
   @Input() showIcon = true; 
+  @Input() icon = 'bi bi-search';
   
   // (text, password, date, email, etc.)
   @Input() type = 'text';
-
   @Input() shape: InputShape = 'square'; 
+
+  // NEU: DaisyUI Size (Default 'md' entspricht ca. 3rem Höhe)
+  @Input() size: InputSize = 'md';
+
+  // NEU: DaisyUI Color Style (Default 'bordered')
+  @Input() color: InputColor = 'bordered';
 
   onInput(event: Event) {
     const target = event.target as HTMLInputElement;
