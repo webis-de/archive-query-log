2fd7763477000200976983e996319ba9854dc3ca
frzwlf <ricorichter02@gmail.com>
2026-01-02 10:53:02 +0100

    fix: lint issues
    Refs: #27

--- projects/aql-stylings/src/lib/components/pagination/aql-pagination.component.spec.ts
+++ projects/aql-stylings/src/lib/components/pagination/aql-pagination.component.spec.ts
@@ -1,10 +1,14 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+
 import { AqlPaginationComponent } from './aql-pagination.component';
+
 import { AqlButtonComponent } from '../button/aql-button.component';
+
 import { TranslateModule } from '@ngx-translate/core';
 
 describe('AqlPaginationComponent', () => {
   let component: AqlPaginationComponent;
+
   let fixture: ComponentFixture<AqlPaginationComponent>;
 
   beforeEach(async () => {
@@ -13,6 +17,7 @@ describe('AqlPaginationComponent', () => {
     }).compileComponents();
 
     fixture = TestBed.createComponent(AqlPaginationComponent);
+
     component = fixture.componentInstance;
   });
 
@@ -22,7 +27,9 @@ describe('AqlPaginationComponent', () => {
 
   it('should calculate total pages correctly', () => {
     fixture.componentRef.setInput('totalItems', 100);
+
     fixture.componentRef.setInput('pageSize', 10);
+
     fixture.detectChanges();
 
     expect(component.totalPages()).toBe(10);
@@ -30,11 +37,15 @@ describe('AqlPaginationComponent', () => {
 
   it('should emit page change event', () => {
     fixture.componentRef.setInput('totalItems', 100);
+
     fixture.componentRef.setInput('pageSize', 10);
+
     fixture.componentRef.setInput('currentPage', 1);
+
     fixture.detectChanges();
 
     let emittedPage: number | undefined;
+
     component.pageChange.subscribe(page => (emittedPage = page));
 
     component.goToPage(3);
@@ -44,11 +55,15 @@ describe('AqlPaginationComponent', () => {
 
   it('should navigate to next page', () => {
     fixture.componentRef.setInput('totalItems', 100);
+
     fixture.componentRef.setInput('pageSize', 10);
+
     fixture.componentRef.setInput('currentPage', 5);
+
     fixture.detectChanges();
 
     let emittedPage: number | undefined;
+
     component.pageChange.subscribe(page => (emittedPage = page));
 
     component.goToNextPage();
