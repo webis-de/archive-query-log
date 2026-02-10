7dde0b990995b9f46e387847e0721a7cf93c954b
Jan Heinrich Merker <heinrich.merker@uni-jena.de>
2026-02-10 15:31:44 +0100

    Fix Angular environment config

--- browser/src/environments/environment.ts
+++ browser/src/environments/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
   // Assume that both the API and web app are routed on the same hostname.
-  apiUrl: '/api',
+  apiUrl: '/',
   apiTimeout: 60000,
 };
