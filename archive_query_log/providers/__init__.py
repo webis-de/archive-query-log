7642f07913f8b95eed955f21e1dab1be58b09b14
Jan Heinrich Merker <heinrich.merker@uni-jena.de>
2026-08-26 13:30:17 +0200

    Remove workaround for now
    Signed-off-by: Jan Heinrich Merker <heinrich.merker@uni-jena.de>

--- archive_query_log/providers/__init__.py
+++ archive_query_log/providers/__init__.py
@@ -91,7 +91,6 @@ def add_provider(
 
     provider = Provider(
         id=provider_id,
-        index=config.es.index_providers,
         last_modified=last_modified,
         name=name,
         description=description,
@@ -103,6 +102,6 @@ def add_provider(
         should_build_sources=should_build_sources,
     )
     if not dry_run:
-        config.es.bulk([provider.index_action()])
+        provider.save(using=config.es.client, index=config.es.index_providers)
     else:
         print(provider)
